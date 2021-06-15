27d7f2ddf02d25f6110b1bf54d8ac14b215b5929
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-15 01:30:35 -0700

    Implement UMLAL and UMLALS

--- emulator/cpu/arm7tdmi/instructions/multiply_long.h
+++ emulator/cpu/arm7tdmi/instructions/multiply_long.h
@@ -24,4 +24,28 @@ static inline void ArmUMULLS(ArmUserRegisters *registers, ArmRegisterIndex RdLo,
   registers->cpsr.negative = !!(registers->gprs.gprs[RdHi] & (1 << 31));
 }
 
+static inline void ArmUMLAL(ArmGeneralPurposeRegisters *registers,
+                            ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
+                            ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
+  uint64_t result =
+      ((uint64_t)registers->gprs[RdHi] << 32) + (uint64_t)registers->gprs[RdLo];
+  result += (uint64_t)registers->gprs[Rm] * (uint64_t)registers->gprs[Rs];
+  registers->gprs[RdLo] = (uint32_t)result;
+  registers->gprs[RdHi] = (uint32_t)(result >> 32);
+}
+
+static inline void ArmUMLALS(ArmUserRegisters *registers, ArmRegisterIndex RdLo,
+                             ArmRegisterIndex RdHi, ArmRegisterIndex Rm,
+                             ArmRegisterIndex Rs) {
+  uint64_t result = ((uint64_t)registers->gprs.gprs[RdHi] << 32) +
+                    (uint64_t)registers->gprs.gprs[RdLo];
+  result +=
+      (uint64_t)registers->gprs.gprs[Rm] * (uint64_t)registers->gprs.gprs[Rs];
+  registers->gprs.gprs[RdLo] = (uint32_t)result;
+  registers->gprs.gprs[RdHi] = (uint32_t)(result >> 32);
+  registers->cpsr.zero =
+      ((registers->gprs.gprs[RdLo] | registers->gprs.gprs[RdHi]) == 0);
+  registers->cpsr.negative = !!(registers->gprs.gprs[RdHi] & (1 << 31));
+}
+
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_MULTIPLY_LONG_
--- emulator/cpu/arm7tdmi/instructions/multiply_long_test.cc
+++ emulator/cpu/arm7tdmi/instructions/multiply_long_test.cc
@@ -119,3 +119,97 @@ TEST(ArmUMULLS, Negative) {
   registers.cpsr.negative = false;
   EXPECT_TRUE(ArmUserRegistersAreZero(registers));
 }
+
+TEST(ArmUMLAL, Compute) {
+  auto registers = CreateArmGeneralPurposeRegistersRegisters();
+
+  registers.r0 = 65537u;
+  registers.r1 = 65539u;
+  registers.r3 = 1;
+  registers.r2 = 1;
+  ArmUMLAL(&registers, REGISTER_R3, REGISTER_R2, REGISTER_R0, REGISTER_R1);
+  EXPECT_EQ(65537u, registers.r0);
+  EXPECT_EQ(65539u, registers.r1);
+  EXPECT_EQ(2u, registers.r2);
+  EXPECT_EQ(262148u, registers.r3);
+
+  registers.r0 = 0u;
+  registers.r1 = 0u;
+  registers.r2 = 0u;
+  registers.r3 = 0u;
+  EXPECT_TRUE(ArmGeneralPurposeRegistersAreZero(registers));
+}
+
+TEST(ArmUMLAL, ReuseSourceAndDest) {
+  auto registers = CreateArmGeneralPurposeRegistersRegisters();
+
+  registers.r0 = 65537u;
+  registers.r1 = 1u;
+  ArmUMLAL(&registers, REGISTER_R0, REGISTER_R1, REGISTER_R0, REGISTER_R1);
+  EXPECT_EQ(131074u, registers.r0);
+  EXPECT_EQ(1u, registers.r1);
+
+  registers.r0 = 0u;
+  registers.r1 = 0u;
+  EXPECT_TRUE(ArmGeneralPurposeRegistersAreZero(registers));
+}
+
+TEST(ArmUMLALS, Compute) {
+  auto registers = CreateArmUserRegisters();
+
+  registers.gprs.r0 = 65537u;
+  registers.gprs.r1 = 65539u;
+  registers.gprs.r3 = 1u;
+  registers.gprs.r2 = 1u;
+  ArmUMLALS(&registers, REGISTER_R3, REGISTER_R2, REGISTER_R0, REGISTER_R1);
+  EXPECT_EQ(65537u, registers.gprs.r0);
+  EXPECT_EQ(65539u, registers.gprs.r1);
+  EXPECT_EQ(2u, registers.gprs.r2);
+  EXPECT_EQ(262148u, registers.gprs.r3);
+
+  registers.gprs.r0 = 0u;
+  registers.gprs.r1 = 0u;
+  registers.gprs.r2 = 0u;
+  registers.gprs.r3 = 0u;
+  EXPECT_TRUE(ArmUserRegistersAreZero(registers));
+}
+
+TEST(ArmUMLALS, ReuseSourceAndDest) {
+  auto registers = CreateArmUserRegisters();
+
+  registers.gprs.r0 = 65537u;
+  registers.gprs.r1 = 1u;
+  ArmUMLALS(&registers, REGISTER_R0, REGISTER_R1, REGISTER_R0, REGISTER_R1);
+  EXPECT_EQ(131074u, registers.gprs.r0);
+  EXPECT_EQ(1u, registers.gprs.r1);
+
+  registers.gprs.r0 = 0u;
+  registers.gprs.r1 = 0u;
+  EXPECT_TRUE(ArmUserRegistersAreZero(registers));
+}
+
+TEST(ArmUMLALS, Zero) {
+  auto registers = CreateArmUserRegisters();
+
+  registers.gprs.r0 = 65537u;
+  ArmUMLALS(&registers, REGISTER_R3, REGISTER_R2, REGISTER_R0, REGISTER_R1);
+  EXPECT_EQ(65537u, registers.gprs.r0);
+  EXPECT_TRUE(registers.cpsr.zero);
+
+  registers.gprs.r0 = 0u;
+  registers.cpsr.zero = false;
+  EXPECT_TRUE(ArmUserRegistersAreZero(registers));
+}
+
+TEST(ArmUMLALS, Negative) {
+  auto registers = CreateArmUserRegisters();
+
+  registers.gprs.r0 = 0xFFFFFFFF;
+  ArmUMLALS(&registers, REGISTER_R1, REGISTER_R0, REGISTER_R2, REGISTER_R3);
+  EXPECT_EQ(0xFFFFFFFF, registers.gprs.r0);
+  EXPECT_TRUE(registers.cpsr.negative);
+
+  registers.gprs.r0 = 0u;
+  registers.cpsr.negative = false;
+  EXPECT_TRUE(ArmUserRegistersAreZero(registers));
+}
