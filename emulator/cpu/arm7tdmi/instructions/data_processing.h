5da6dbd47aaf3f4816ba5fe8fc0811241a75536a
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-18 03:08:22 -0700

    Improve code reuse

--- emulator/cpu/arm7tdmi/instructions/data_processing.h
+++ emulator/cpu/arm7tdmi/instructions/data_processing.h
@@ -3,22 +3,23 @@
 
 #include "emulator/cpu/arm7tdmi/arm7tdmi.h"
 
-static inline void ArmADD(ArmGeneralPurposeRegisters *registers,
-                          ArmRegisterIndex Rd, ArmRegisterIndex Rn,
-                          uint32_t operand2) {
-  registers->gprs[Rd] = registers->gprs[Rn] + operand2;
+static inline uint64_t ArmADD(ArmGeneralPurposeRegisters *registers,
+                              ArmRegisterIndex Rd, ArmRegisterIndex Rn,
+                              uint32_t operand2) {
+  uint64_t sum = (uint64_t)registers->gprs[Rn] + (uint64_t)operand2;
+  registers->gprs[Rd] = (uint32_t)sum;
+  return sum;
 }
 
 static inline void ArmADDS(ArmUserRegisters *registers, ArmRegisterIndex Rd,
                            ArmRegisterIndex Rn, uint32_t operand2) {
-  uint64_t sum = (uint64_t)registers->gprs.gprs[Rn] + (uint64_t)operand2;
   int64_t sum_s =
       (int64_t)registers->gprs.gprs_s[Rn] + (int64_t)(int32_t)operand2;
-  registers->gprs.gprs[Rd] = (uint32_t)sum;
-  registers->cpsr.negative = ((int32_t)sum_s < 0);
-  registers->cpsr.zero = (sum == 0);
-  registers->cpsr.carry = (sum > UINT32_MAX);
-  registers->cpsr.overflow = !!((sum_s > INT32_MAX) | (sum_s < INT32_MIN));
+  uint64_t sum = ArmADD(&registers->gprs, Rd, Rn, operand2);
+  registers->cpsr.negative = ArmNegativeFlagUInt32(registers->gprs.gprs_s[Rd]);
+  registers->cpsr.zero = ArmZeroFlagUInt64(sum);
+  registers->cpsr.carry = ArmCarryFlag(sum);
+  registers->cpsr.overflow = ArmOverflowFlag(sum_s);
 }
 
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_DATA_PROCESSING_
--- emulator/cpu/arm7tdmi/instructions/multiply_long.h
+++ emulator/cpu/arm7tdmi/instructions/multiply_long.h
@@ -3,90 +3,78 @@
 
 #include "emulator/cpu/arm7tdmi/arm7tdmi.h"
 
-static inline void ArmUMULL(ArmGeneralPurposeRegisters *registers,
-                            ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
-                            ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
+static inline uint64_t ArmUMULL(ArmGeneralPurposeRegisters *registers,
+                                ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
+                                ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
   uint64_t product =
       (uint64_t)registers->gprs[Rm] * (uint64_t)registers->gprs[Rs];
   registers->gprs[RdLo] = (uint32_t)product;
   registers->gprs[RdHi] = (uint32_t)(product >> 32);
+  return product;
 }
 
 static inline void ArmUMULLS(ArmUserRegisters *registers, ArmRegisterIndex RdLo,
                              ArmRegisterIndex RdHi, ArmRegisterIndex Rm,
                              ArmRegisterIndex Rs) {
-  uint64_t product =
-      (uint64_t)registers->gprs.gprs[Rm] * (uint64_t)registers->gprs.gprs[Rs];
-  registers->gprs.gprs[RdLo] = (uint32_t)product;
-  registers->gprs.gprs[RdHi] = (uint32_t)(product >> 32);
-  registers->cpsr.zero = (product == 0);
-  registers->cpsr.negative = ((int64_t)product < 0);
+  uint64_t product = ArmUMULL(&registers->gprs, RdLo, RdHi, Rm, Rs);
+  registers->cpsr.zero = ArmZeroFlagUInt64(product);
+  registers->cpsr.negative = ArmNegativeFlagUInt64(product);
 }
 
-static inline void ArmUMLAL(ArmGeneralPurposeRegisters *registers,
-                            ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
-                            ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
+static inline uint64_t ArmUMLAL(ArmGeneralPurposeRegisters *registers,
+                                ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
+                                ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
   uint64_t result =
       ((uint64_t)registers->gprs[RdHi] << 32) + (uint64_t)registers->gprs[RdLo];
   result += (uint64_t)registers->gprs[Rm] * (uint64_t)registers->gprs[Rs];
   registers->gprs[RdLo] = (uint32_t)result;
   registers->gprs[RdHi] = (uint32_t)(result >> 32);
+  return result;
 }
 
 static inline void ArmUMLALS(ArmUserRegisters *registers, ArmRegisterIndex RdLo,
                              ArmRegisterIndex RdHi, ArmRegisterIndex Rm,
                              ArmRegisterIndex Rs) {
-  uint64_t result = ((uint64_t)registers->gprs.gprs[RdHi] << 32) +
-                    (uint64_t)registers->gprs.gprs[RdLo];
-  result +=
-      (uint64_t)registers->gprs.gprs[Rm] * (uint64_t)registers->gprs.gprs[Rs];
-  registers->gprs.gprs[RdLo] = (uint32_t)result;
-  registers->gprs.gprs[RdHi] = (uint32_t)(result >> 32);
-  registers->cpsr.zero = (result == 0);
-  registers->cpsr.negative = ((int64_t)result < 0);
+  uint64_t result = ArmUMLAL(&registers->gprs, RdLo, RdHi, Rm, Rs);
+  registers->cpsr.zero = ArmZeroFlagUInt64(result);
+  registers->cpsr.negative = ArmNegativeFlagUInt64(result);
 }
 
-static inline void ArmSMULL(ArmGeneralPurposeRegisters *registers,
-                            ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
-                            ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
+static inline int64_t ArmSMULL(ArmGeneralPurposeRegisters *registers,
+                               ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
+                               ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
   int64_t product =
       (int64_t)registers->gprs_s[Rm] * (int64_t)registers->gprs_s[Rs];
   registers->gprs[RdLo] = (uint32_t)(uint64_t)product;
   registers->gprs[RdHi] = (uint32_t)((uint64_t)product >> 32);
+  return product;
 }
 
 static inline void ArmSMULLS(ArmUserRegisters *registers, ArmRegisterIndex RdLo,
                              ArmRegisterIndex RdHi, ArmRegisterIndex Rm,
                              ArmRegisterIndex Rs) {
-  int64_t product =
-      (int64_t)registers->gprs.gprs_s[Rm] * (int64_t)registers->gprs.gprs_s[Rs];
-  registers->gprs.gprs[RdLo] = (uint32_t)(uint64_t)product;
-  registers->gprs.gprs[RdHi] = (uint32_t)((uint64_t)product >> 32);
-  registers->cpsr.zero = (product == 0);
-  registers->cpsr.negative = (product < 0);
+  int64_t product = ArmSMULL(&registers->gprs, RdLo, RdHi, Rm, Rs);
+  registers->cpsr.zero = ArmZeroFlagInt64(product);
+  registers->cpsr.negative = ArmNegativeFlagInt64(product);
 }
 
-static inline void ArmSMLAL(ArmGeneralPurposeRegisters *registers,
-                            ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
-                            ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
+static inline int64_t ArmSMLAL(ArmGeneralPurposeRegisters *registers,
+                               ArmRegisterIndex RdLo, ArmRegisterIndex RdHi,
+                               ArmRegisterIndex Rm, ArmRegisterIndex Rs) {
   int64_t result = (int64_t)(((uint64_t)registers->gprs[RdHi] << 32) +
                              (uint64_t)registers->gprs[RdLo]);
   result += (int64_t)registers->gprs_s[Rm] * (int64_t)registers->gprs_s[Rs];
   registers->gprs[RdLo] = (uint32_t)(uint64_t)result;
   registers->gprs[RdHi] = (uint32_t)((uint64_t)result >> 32);
+  return result;
 }
 
 static inline void ArmSMLALS(ArmUserRegisters *registers, ArmRegisterIndex RdLo,
                              ArmRegisterIndex RdHi, ArmRegisterIndex Rm,
                              ArmRegisterIndex Rs) {
-  int64_t result = (int64_t)(((uint64_t)registers->gprs.gprs[RdHi] << 32) +
-                             (uint64_t)registers->gprs.gprs[RdLo]);
-  result +=
-      (int64_t)registers->gprs.gprs_s[Rm] * (int64_t)registers->gprs.gprs_s[Rs];
-  registers->gprs.gprs[RdLo] = (uint32_t)(uint64_t)result;
-  registers->gprs.gprs[RdHi] = (uint32_t)((uint64_t)result >> 32);
-  registers->cpsr.zero = (result == 0);
-  registers->cpsr.negative = (result < 0);
+  int64_t result = ArmSMLAL(&registers->gprs, RdLo, RdHi, Rm, Rs);
+  registers->cpsr.zero = ArmZeroFlagInt64(result);
+  registers->cpsr.negative = ArmNegativeFlagInt64(result);
 }
 
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_MULTIPLY_LONG_
