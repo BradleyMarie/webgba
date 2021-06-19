43727008065e7e689b8027b8b653c4b3d7adf3ef
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-18 22:40:53 -0700

    Implement AND and ANDS

--- emulator/cpu/arm7tdmi/instructions/data_processing.h
+++ emulator/cpu/arm7tdmi/instructions/data_processing.h
@@ -35,10 +35,24 @@ static inline void ArmADDS(ArmUserRegisters *registers, ArmRegisterIndex Rd,
   int64_t sum_s =
       (int64_t)registers->gprs.gprs_s[Rn] + (int64_t)(int32_t)operand2;
   uint64_t sum = ArmADD(&registers->gprs, Rd, Rn, operand2);
-  registers->cpsr.negative = ArmNegativeFlagUInt32(registers->gprs.gprs_s[Rd]);
-  registers->cpsr.zero = ArmZeroFlagUInt32(registers->gprs.gprs_s[Rd]);
+  registers->cpsr.negative = ArmNegativeFlagUInt32(registers->gprs.gprs[Rd]);
+  registers->cpsr.zero = ArmZeroFlagUInt32(registers->gprs.gprs[Rd]);
   registers->cpsr.carry = ArmCarryFlag(sum);
   registers->cpsr.overflow = ArmOverflowFlag(sum_s);
+}
+
+static inline void ArmAND(ArmGeneralPurposeRegisters *registers,
+                          ArmRegisterIndex Rd, ArmRegisterIndex Rn,
+                          uint32_t operand2) {
+  registers->gprs[Rd] = registers->gprs[Rn] & operand2;
+}
+
+static inline void ArmANDS(ArmUserRegisters *registers, ArmRegisterIndex Rd,
+                           ArmRegisterIndex Rn, uint32_t operand2, bool carry) {
+  ArmAND(&registers->gprs, Rd, Rn, operand2);
+  registers->cpsr.negative = ArmNegativeFlagUInt32(registers->gprs.gprs[Rd]);
+  registers->cpsr.zero = ArmZeroFlagUInt32(registers->gprs.gprs[Rd]);
+  registers->cpsr.carry = carry;
 }
 
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_DATA_PROCESSING_
