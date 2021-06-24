c1e3e4862eadc25034cd0fc7bc040b5cb788e2e8
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-23 23:56:02 -0700

    Implement STRSB

--- emulator/cpu/arm7tdmi/instructions/signed_data_transfer.h
+++ emulator/cpu/arm7tdmi/instructions/signed_data_transfer.h
@@ -195,4 +195,48 @@ static inline void ArmSTRH_IncrementPostIndexed(
   registers->gprs[Rn] += offset;
 }
 
+static inline void ArmSTRSB(ArmGeneralPurposeRegisters *registers,
+                            Memory *memory, ArmRegisterIndex Rd,
+                            uint32_t address) {
+  int8_t temp = registers->gprs_s[Rd];
+  bool success = Store8S(memory, address, temp);
+  assert(success);
+}
+
+static inline void ArmSTRSB_DecrementPreIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  registers->gprs[Rn] -= offset;
+  int8_t temp = registers->gprs_s[Rd];
+  bool success = Store8S(memory, registers->gprs[Rn], temp);
+  assert(success);
+}
+
+static inline void ArmSTRSB_DecrementPostIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  int8_t temp = registers->gprs_s[Rd];
+  bool success = Store8S(memory, registers->gprs[Rn], temp);
+  assert(success);
+  registers->gprs[Rn] -= offset;
+}
+
+static inline void ArmSTRSB_IncrementPreIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  registers->gprs[Rn] += offset;
+  int8_t temp = registers->gprs_s[Rd];
+  bool success = Store8S(memory, registers->gprs[Rn], temp);
+  assert(success);
+}
+
+static inline void ArmSTRSB_IncrementPostIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  int8_t temp = registers->gprs_s[Rd];
+  bool success = Store8S(memory, registers->gprs[Rn], temp);
+  assert(success);
+  registers->gprs[Rn] += offset;
+}
+
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_SIGNED_DATA_TRANSFER_
