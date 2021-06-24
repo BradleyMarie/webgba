1c356acdbcf29e0fffcaf15bbcc038c3f448631b
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-23 23:58:39 -0700

    Implement STRSH

--- emulator/cpu/arm7tdmi/instructions/signed_data_transfer.h
+++ emulator/cpu/arm7tdmi/instructions/signed_data_transfer.h
@@ -239,4 +239,48 @@ static inline void ArmSTRSB_IncrementPostIndexed(
   registers->gprs[Rn] += offset;
 }
 
+static inline void ArmSTRSH(ArmGeneralPurposeRegisters *registers,
+                            Memory *memory, ArmRegisterIndex Rd,
+                            uint32_t address) {
+  int16_t temp = registers->gprs_s[Rd];
+  bool success = Store16SLE(memory, address, temp);
+  assert(success);
+}
+
+static inline void ArmSTRSH_DecrementPreIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  registers->gprs[Rn] -= offset;
+  int16_t temp = registers->gprs_s[Rd];
+  bool success = Store16SLE(memory, registers->gprs[Rn], temp);
+  assert(success);
+}
+
+static inline void ArmSTRSH_DecrementPostIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  int16_t temp = registers->gprs_s[Rd];
+  bool success = Store16SLE(memory, registers->gprs[Rn], temp);
+  assert(success);
+  registers->gprs[Rn] -= offset;
+}
+
+static inline void ArmSTRSH_IncrementPreIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  registers->gprs[Rn] += offset;
+  int16_t temp = registers->gprs_s[Rd];
+  bool success = Store16SLE(memory, registers->gprs[Rn], temp);
+  assert(success);
+}
+
+static inline void ArmSTRSH_IncrementPostIndexed(
+    ArmGeneralPurposeRegisters *registers, Memory *memory, ArmRegisterIndex Rd,
+    ArmRegisterIndex Rn, uint_fast16_t offset) {
+  int16_t temp = registers->gprs_s[Rd];
+  bool success = Store16SLE(memory, registers->gprs[Rn], temp);
+  assert(success);
+  registers->gprs[Rn] += offset;
+}
+
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_SIGNED_DATA_TRANSFER_
