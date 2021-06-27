6ebae090224545cc883e3bcb05ecdaf2cc3abc46
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-26 23:08:12 -0700

    Add stubs for coprocessor functions

---
+++ emulator/cpu/arm7tdmi/instructions/coprocessor_data_transfer.h
@@ -0,0 +1,16 @@
+#ifndef _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_DATA_TRANSFER_
+#define _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_DATA_TRANSFER_
+
+#include <assert.h>
+
+#include "emulator/cpu/arm7tdmi/arm7tdmi.h"
+
+static inline void ArmLDC(ArmAllRegisters *registers) {
+  // NOP
+}
+
+static inline void ArmSTC(ArmAllRegisters *registers) {
+  // NOP
+}
+
+#endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_DATA_TRANSFER_
---
+++ emulator/cpu/arm7tdmi/instructions/coprocessor_register_transfer.h
@@ -0,0 +1,16 @@
+#ifndef _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_REGISTER_TRANSFER_
+#define _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_REGISTER_TRANSFER_
+
+#include <assert.h>
+
+#include "emulator/cpu/arm7tdmi/arm7tdmi.h"
+
+static inline void ArmMCR(ArmAllRegisters *registers) {
+  // NOP
+}
+
+static inline void ArmMRC(ArmAllRegisters *registers) {
+  // NOP
+}
+
+#endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_REGISTER_TRANSFER_
