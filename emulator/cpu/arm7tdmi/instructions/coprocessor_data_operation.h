69d80811178f4a74953283b108ed3c59e58da8da
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-06-26 23:08:12 -0700

    Add stubs for coprocessor functions

---
+++ emulator/cpu/arm7tdmi/instructions/coprocessor_data_operation.h
@@ -0,0 +1,12 @@
+#ifndef _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_DATA_OPERATION_
+#define _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_DATA_OPERATION_
+
+#include <assert.h>
+
+#include "emulator/cpu/arm7tdmi/arm7tdmi.h"
+
+static inline void ArmCDP(ArmAllRegisters *registers) {
+  // NOP
+}
+
+#endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_COPROCESSOR_DATA_OPERATION_
