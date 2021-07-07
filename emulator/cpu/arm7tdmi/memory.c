702d890be95de90876f4758995803f883a48c467
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-07 00:06:41 -0700

    Use constants for masks

--- emulator/cpu/arm7tdmi/memory.c
+++ emulator/cpu/arm7tdmi/memory.c
@@ -27,64 +27,64 @@ static inline void RotateDataByAddress(uint32_t address, uint32_t *value) {
 
 bool ArmLoad32LEWithRotation(const Memory *memory, uint32_t address,
                              uint32_t *value) {
-  uint32_t masked_address = address & (~3u);
+  uint32_t masked_address = address & 0xFFFFFFFCu;
   bool result = Load32LE(memory, masked_address, value);
   RotateDataByAddress(address, value);
   return result;
 }
 
 bool ArmLoad32LE(const Memory *memory, uint32_t address, uint32_t *value) {
-  uint32_t masked_address = address & (~3u);
+  uint32_t masked_address = address & 0xFFFFFFFCu;
   bool result = Load32LE(memory, masked_address, value);
   return result;
 }
 
 // Unaligned 16 bit reads have unpredictable behavior in the ARM
 bool ArmLoad16LE(const Memory *memory, uint32_t address, uint16_t *value) {
-  uint32_t masked_address = address & (~1u);
+  uint32_t masked_address = address & 0xFFFFFFFEu;
   bool result = Load16LE(memory, masked_address, value);
   return result;
 }
 
 bool ArmLoad32SLEWithRotation(const Memory *memory, uint32_t address,
                               int32_t *value) {
-  uint32_t masked_address = address & (~3u);
+  uint32_t masked_address = address & 0xFFFFFFFCu;
   bool result = Load32SLE(memory, masked_address, value);
   RotateDataByAddress(address, (uint32_t *)value);
   return result;
 }
 
 bool ArmLoad32SLE(const Memory *memory, uint32_t address, int32_t *value) {
-  uint32_t masked_address = address & (~3u);
+  uint32_t masked_address = address & 0xFFFFFFFCu;
   bool result = Load32SLE(memory, masked_address, value);
   return result;
 }
 
 // Unaligned 16 bit reads have unpredictable behavior in the ARM
 bool ArmLoad16SLE(const Memory *memory, uint32_t address, int16_t *value) {
-  uint32_t masked_address = address & (~1u);
+  uint32_t masked_address = address & 0xFFFFFFFEu;
   bool result = Load16SLE(memory, masked_address, value);
   return result;
 }
 
 bool ArmStore32LE(Memory *memory, uint32_t address, uint32_t value) {
-  uint32_t masked_address = address & (~3u);
+  uint32_t masked_address = address & 0xFFFFFFFCu;
   return Store32LE(memory, masked_address, value);
 }
 
 // Unaligned 16 bit reads have unpredictable behavior in the ARM
 bool ArmStore16LE(Memory *memory, uint32_t address, uint16_t value) {
-  uint32_t masked_address = address & (~1u);
+  uint32_t masked_address = address & 0xFFFFFFFEu;
   return Store16LE(memory, masked_address, value);
 }
 
 bool ArmStore32SLE(Memory *memory, uint32_t address, int32_t value) {
-  uint32_t masked_address = address & (~3u);
+  uint32_t masked_address = address & 0xFFFFFFFCu;
   return Store32SLE(memory, masked_address, value);
 }
 
 // Unaligned 16 bit reads have unpredictable behavior in the ARM
 bool ArmStore16SLE(Memory *memory, uint32_t address, int16_t value) {
-  uint32_t masked_address = address & (~1u);
+  uint32_t masked_address = address & 0xFFFFFFFEu;
   return Store16SLE(memory, masked_address, value);
 }
