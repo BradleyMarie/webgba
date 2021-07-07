47bcdc2ec0390a87dcc566c2f1daef1e27eb2c29
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-07 00:06:41 -0700

    Use constants for masks

--- emulator/cpu/arm7tdmi/instructions/branch_exchange.h
+++ emulator/cpu/arm7tdmi/instructions/branch_exchange.h
@@ -5,7 +5,7 @@
 
 static inline void ArmBX(ArmUserRegisters *registers, ArmRegisterIndex Rm) {
   registers->cpsr.thumb = registers->gprs.gprs[Rm] & 1u;
-  registers->gprs.pc = registers->gprs.gprs[Rm] & (~1u);
+  registers->gprs.pc = registers->gprs.gprs[Rm] & 0xFFFFFFFEu;
 }
 
 #endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_INSTRUCTIONS_BRANCH_EXCHANGE_
