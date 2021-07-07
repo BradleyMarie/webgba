826226eb33bbace600bc155abd45c6f667762ed4
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-07 02:25:54 -0700

    Add operand tests

--- emulator/cpu/arm7tdmi/decoders/thumb/operand.h
+++ emulator/cpu/arm7tdmi/decoders/thumb/operand.h
@@ -3,16 +3,19 @@
 
 #include "emulator/cpu/arm7tdmi/arm7tdmi.h"
 
-static inline void ThumbOperandShift(uint16_t instruction, ArmRegisterIndex *Rd,
-                                     ArmRegisterIndex *Rm) {
+static inline void ThumbOperandShiftByImmediate(uint16_t instruction,
+                                                ArmRegisterIndex *Rd,
+                                                ArmRegisterIndex *Rm,
+                                                uint_fast8_t *immediate) {
   *Rd = (ArmRegisterIndex)((instruction >> 0u) & 0x7u);
   *Rm = (ArmRegisterIndex)((instruction >> 3u) & 0x7u);
+  *immediate = ((instruction >> 6u) & 0x1Fu);
 }
 
 static inline void ThumbOperandAddSubtractRegister(uint16_t instruction,
                                                    ArmRegisterIndex *Rd,
-                                                   ArmRegisterIndex *Rm,
-                                                   ArmRegisterIndex *Rn) {
+                                                   ArmRegisterIndex *Rn,
+                                                   ArmRegisterIndex *Rm) {
   *Rd = (ArmRegisterIndex)((instruction >> 0u) & 0x7u);
   *Rn = (ArmRegisterIndex)((instruction >> 3u) & 0x7u);
   *Rm = (ArmRegisterIndex)((instruction >> 6u) & 0x7u);
@@ -43,17 +46,14 @@ static inline void ThumbOperandDataProcessingRegister(uint16_t instruction,
 static inline void ThumbOperandSpecialDataProcessing(uint16_t instruction,
                                                      ArmRegisterIndex *Rd_Rn,
                                                      ArmRegisterIndex *Rm) {
-  bool h1 = (instruction >> 7u) & 1u;
-  bool h2 = (instruction >> 6u) & 1u;
-
-  *Rd_Rn = (ArmRegisterIndex)(((instruction >> 0u) & 0x7u) | (h1 << 4u));
-  *Rm = (ArmRegisterIndex)(((instruction >> 3u) & 0x7u) | (h2 << 4u));
+  *Rd_Rn = (ArmRegisterIndex)(((instruction >> 3u) & 0x8u) |
+                              ((instruction >> 0u) & 0x7u));
+  *Rm = (ArmRegisterIndex)((instruction >> 3u) & 0xFu);
 }
 
 static inline void ThumbOperandBranchExchange(uint16_t instruction,
                                               ArmRegisterIndex *Rm) {
-  bool h2 = (instruction >> 6u) & 1u;
-  *Rm = (ArmRegisterIndex)(((instruction >> 3u) & 0x7u) | (h2 << 4u));
+  *Rm = (ArmRegisterIndex)((instruction >> 3u) & 0xFu);
 }
 
 static inline void ThumbOperandLoadPCRelative(uint16_t instruction,
@@ -65,52 +65,42 @@ static inline void ThumbOperandLoadPCRelative(uint16_t instruction,
 
 static inline void ThumbOperandLoadStoreRegisterOffset(uint16_t instruction,
                                                        ArmRegisterIndex *Rd,
-                                                       ArmRegisterIndex *Rm,
-                                                       ArmRegisterIndex *Rn) {
+                                                       ArmRegisterIndex *Rn,
+                                                       ArmRegisterIndex *Rm) {
   *Rd = (ArmRegisterIndex)((instruction >> 0u) & 0x7u);
   *Rn = (ArmRegisterIndex)((instruction >> 3u) & 0x7u);
   *Rm = (ArmRegisterIndex)((instruction >> 6u) & 0x7u);
 }
 
-static inline void ThumbOperandLoadStoreWordByteImmediateOffset(
-    uint16_t instruction, ArmRegisterIndex *Rd, ArmRegisterIndex *Rn,
-    uint_fast8_t *offset) {
+static inline void ThumbOperandLoadStoreImmediateOffset(uint16_t instruction,
+                                                        ArmRegisterIndex *Rd,
+                                                        ArmRegisterIndex *Rn,
+                                                        uint_fast8_t *offset) {
   *Rd = (ArmRegisterIndex)((instruction >> 0u) & 0x7u);
   *Rn = (ArmRegisterIndex)((instruction >> 3u) & 0x7u);
-  *offset = (instruction >> 6u) & 0x1Fu;
-}
-
-static inline void ThumbOperandLoadStoreHalfWordImmediateOffset(
-    uint16_t instruction, ArmRegisterIndex *Rd, ArmRegisterIndex *Rn,
-    uint_fast8_t *offset) {
-  *Rd = (ArmRegisterIndex)((instruction >> 0u) & 0x7u);
-  *Rn = (ArmRegisterIndex)((instruction >> 3u) & 0x7u);
-  *offset = (instruction >> 6u) & 0x1Fu;
+  *offset = ((instruction >> 6u) & 0x1Fu) * 4u;
 }
 
 static inline void ThumbOperandLoadStoreSPRelative(uint16_t instruction,
                                                    ArmRegisterIndex *Rd,
                                                    uint_fast16_t *offset) {
-  *Rd = (ArmRegisterIndex)((instruction >> 8u) & 0x7u);
-  *offset = (instruction & 0xFFu) * 4u;
+  ThumbOperandLoadPCRelative(instruction, Rd, offset);
 }
 
 static inline void ThumbOperandAddToSPOrPC(uint16_t instruction,
                                            ArmRegisterIndex *Rd,
                                            uint_fast16_t *offset) {
-  *Rd = (ArmRegisterIndex)((instruction >> 8u) & 0x7u);
-  *offset = (instruction & 0xFFu) * 4u;
+  ThumbOperandLoadPCRelative(instruction, Rd, offset);
 }
 
-static inline void ThumbAdjustStackPointer(uint16_t instruction,
-                                           uint_fast8_t *immediate) {
-  *immediate = instruction & 0x7Fu;
+static inline void ThumbOperandAdjustStackPointer(uint16_t instruction,
+                                                  uint_fast16_t *immediate) {
+  *immediate = (instruction & 0x7Fu) * 4u;
 }
 
-static inline void ThumbPushPopRegisterList(uint16_t instruction,
-                                            uint_fast16_t *register_list) {
-  bool lr = (instruction >> 8u) & 1u;
-  *register_list = instruction & 0x7Fu | (lr << REGISTER_R14);
+static inline void ThumbOperandPushPopRegisterList(
+    uint16_t instruction, uint_fast16_t *register_list) {
+  *register_list = ((instruction & 0x100) << 6u) | (instruction & 0xFFu);
 }
 
 static inline void ThumbOperandLoadStoreMultiple(uint16_t instruction,
--- emulator/cpu/arm7tdmi/decoders/thumb/operand_test.cc
+++ emulator/cpu/arm7tdmi/decoders/thumb/operand_test.cc
@@ -5,3 +5,189 @@ extern "C" {
 #include <cstring>
 
 #include "googletest/include/gtest/gtest.h"
+
+TEST(ThumbShiftByImmediate, Compute) {
+  uint16_t instruction = 0x0148u;  // lsls r0, r1, #5
+  ArmRegisterIndex rd, rm;
+  uint_fast8_t immediate;
+  ThumbOperandShiftByImmediate(instruction, &rd, &rm, &immediate);
+  EXPECT_EQ(REGISTER_R0, rd);
+  EXPECT_EQ(REGISTER_R1, rm);
+  EXPECT_EQ(5u, immediate);
+}
+
+TEST(ThumbAddSubtractRegister, Compute) {
+  uint16_t instruction = 0x1888u;  // adds r0, r1, r2
+  ArmRegisterIndex rd, rn, rm;
+  ThumbOperandAddSubtractRegister(instruction, &rd, &rn, &rm);
+  EXPECT_EQ(REGISTER_R0, rd);
+  EXPECT_EQ(REGISTER_R1, rn);
+  EXPECT_EQ(REGISTER_R2, rm);
+}
+
+TEST(ThumbAddSubtractImmediate, Compute) {
+  uint16_t instruction = 0x1C88;  // adds r0, r1, #2
+  ArmRegisterIndex rd, rn;
+  uint_fast8_t immediate;
+  ThumbOperandAddSubtractImmediate(instruction, &rd, &rn, &immediate);
+  EXPECT_EQ(REGISTER_R0, rd);
+  EXPECT_EQ(REGISTER_R1, rn);
+  EXPECT_EQ(2u, immediate);
+}
+
+TEST(ThumbAddSubtractCompareMoveImmediate, Compute) {
+  uint16_t instruction = 0x2802u;  // cmp r0, #2
+  ArmRegisterIndex rd_rn;
+  uint_fast8_t immediate;
+  ThumbOperandAddSubtractCompareMoveImmediate(instruction, &rd_rn, &immediate);
+  EXPECT_EQ(REGISTER_R0, rd_rn);
+  EXPECT_EQ(2u, immediate);
+}
+
+TEST(ThumbDataProcessingRegister, Compute) {
+  uint16_t instruction = 0x4348;  // muls r0, r0, r1
+  ArmRegisterIndex rd_rn, rm_rs;
+  ThumbOperandDataProcessingRegister(instruction, &rd_rn, &rm_rs);
+  EXPECT_EQ(REGISTER_R0, rd_rn);
+  EXPECT_EQ(REGISTER_R1, rm_rs);
+}
+
+TEST(ThumbSpecialDataProcessing, Compute) {
+  uint16_t instruction = 0x44C8;  // add r8, r8, r9
+  ArmRegisterIndex rd_rn, rm;
+  ThumbOperandSpecialDataProcessing(instruction, &rd_rn, &rm);
+  EXPECT_EQ(REGISTER_R8, rd_rn);
+  EXPECT_EQ(REGISTER_R9, rm);
+}
+
+TEST(ThumbBranchExchange, Compute) {
+  uint16_t instruction = 0x4738u;  // bx r7
+  ArmRegisterIndex rm;
+  ThumbOperandBranchExchange(instruction, &rm);
+  EXPECT_EQ(REGISTER_R7, rm);
+}
+
+TEST(ThumbLoadPCRelative, Compute) {
+  uint16_t instruction = 0x4FFFu;  // ldr r7, [pc, #1020]
+  ArmRegisterIndex rd;
+  uint_fast16_t offset;
+  ThumbOperandLoadPCRelative(instruction, &rd, &offset);
+  EXPECT_EQ(REGISTER_R7, rd);
+  EXPECT_EQ(1020u, offset);
+}
+
+TEST(ThumbLoadStoreRegisterOffset, Compute) {
+  uint16_t instruction = 0x5888;  // ldr r0, [r1, r2]
+  ArmRegisterIndex rd, rn, rm;
+  ThumbOperandLoadStoreRegisterOffset(instruction, &rd, &rn, &rm);
+  EXPECT_EQ(REGISTER_R0, rd);
+  EXPECT_EQ(REGISTER_R1, rn);
+  EXPECT_EQ(REGISTER_R2, rm);
+}
+
+TEST(ThumbLoadStoreImmediateOffset, Compute) {
+  uint16_t instruction = 0x6FC8;  // ldr r0, [r1, #124]
+  ArmRegisterIndex rd, rn;
+  uint_fast8_t offset;
+  ThumbOperandLoadStoreImmediateOffset(instruction, &rd, &rn, &offset);
+  EXPECT_EQ(REGISTER_R0, rd);
+  EXPECT_EQ(REGISTER_R1, rn);
+  EXPECT_EQ(124u, offset);
+}
+
+TEST(ThumbLoadStoreSPRelative, Compute) {
+  uint16_t instruction = 0x9FFF;  // ldr r7, [sp, #1020]
+  ArmRegisterIndex rd;
+  uint_fast16_t offset;
+  ThumbOperandLoadStoreSPRelative(instruction, &rd, &offset);
+  EXPECT_EQ(REGISTER_R7, rd);
+  EXPECT_EQ(1020u, offset);
+}
+
+TEST(ThumbLoadStoreAddToSPOrPc, Compute) {
+  uint16_t instruction = 0xAFFF;  // add r7, sp, #1020
+  ArmRegisterIndex rd;
+  uint_fast16_t offset;
+  ThumbOperandAddToSPOrPC(instruction, &rd, &offset);
+  EXPECT_EQ(REGISTER_R7, rd);
+  EXPECT_EQ(1020u, offset);
+}
+
+TEST(ThumbAdjustStackPointer, Compute) {
+  uint16_t instruction = 0xB07Fu;  // add sp, sp, #508
+  uint_fast16_t immediate;
+  ThumbOperandAdjustStackPointer(instruction, &immediate);
+  EXPECT_EQ(508u, immediate);
+}
+
+TEST(ThumbPushPopRegisterList, Compute) {
+  uint16_t instruction = 0xB5FF;  // push {r0-r7, lr}
+  uint_fast16_t register_list;
+  ThumbOperandPushPopRegisterList(instruction, &register_list);
+  EXPECT_EQ(0x40FFu, register_list);
+}
+
+TEST(ThumbLoadStoreMultiple, Compute) {
+  uint16_t instruction = 0xCFFFu;  // ldm r7, {r0-r7}
+  ArmRegisterIndex rd;
+  uint_fast16_t register_list;
+  ThumbOperandLoadStoreMultiple(instruction, &rd, &register_list);
+  EXPECT_EQ(REGISTER_R7, rd);
+  EXPECT_EQ(0xFFu, register_list);
+}
+
+TEST(ThumbConditionalForwardBranch, Compute) {
+  uint16_t instruction = 0xDD7Du;  // ble #254
+  uint_fast8_t condition;
+  uint_fast32_t offset;
+  ThumbOperandConditionalForwardBranch(instruction, &condition, &offset);
+  EXPECT_EQ(13u, condition);
+  EXPECT_EQ(250u, offset);
+}
+
+TEST(ThumbConditionalReverseBranch, Compute) {
+  uint16_t instruction = 0xDDFFu;  // ble #2
+  uint_fast8_t condition;
+  uint_fast32_t offset;
+  ThumbOperandConditionalReverseBranch(instruction, &condition, &offset);
+  EXPECT_EQ(13u, condition);
+  EXPECT_EQ(0xFFFFFFFEu, offset);
+}
+
+TEST(ThumbForwardBranch, Compute) {
+  uint16_t instruction = 0xE3FDu;  // b #2046
+  uint_fast32_t offset;
+  ThumbOperandForwardBranch(instruction, &offset);
+  EXPECT_EQ(2042u, offset);
+}
+
+TEST(ThumbReverseBranch, Compute) {
+  uint16_t instruction = 0xE7FF;  // b #2
+  uint_fast32_t offset;
+  ThumbOperandReverseBranch(instruction, &offset);
+  EXPECT_EQ(0xFFFFFFFEu, offset);
+}
+
+TEST(ThumbForwardBranchLink, Compute) {
+  // bl #0xFFFE
+  uint_fast32_t offset;
+  uint16_t first_instruction = 0xF00Fu;
+  ThumbOperandForwardBranchLink(first_instruction, &offset);
+  EXPECT_EQ(0xF000u, offset);
+
+  uint16_t second_instruction = 0xFFFDu;
+  ThumbOperandBranchLink(second_instruction, &offset);
+  EXPECT_EQ(0xFFAu, offset);
+}
+
+TEST(ThumbReverseBranchLink, Compute) {
+  // bl #2
+  uint_fast32_t offset;
+  uint16_t first_instruction = 0xF7FFu;
+  ThumbOperandReverseBranchLink(first_instruction, &offset);
+  EXPECT_EQ(0xFFFFF000u, offset);
+
+  uint16_t second_instruction = 0xFFFFu;
+  ThumbOperandBranchLink(second_instruction, &offset);
+  EXPECT_EQ(0xFFEu, offset);
+}
