abe960a160fc787d5343e22c3df7d2edf0fc631c
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-09 14:30:24 -0700

    Add arm7tdmi cpu execute routines

---
+++ emulator/cpu/arm7tdmi/execute.h
@@ -0,0 +1,10 @@
+#ifndef _WEBGBA_EMULATOR_CPU_ARM7TDMI_EXECUTE_
+#define _WEBGBA_EMULATOR_CPU_ARM7TDMI_EXECUTE_
+
+#include "emulator/cpu/arm7tdmi/arm7tdmi.h"
+#include "emulator/memory/memory.h"
+
+void ArmCpuStep(ArmAllRegisters* registers, Memory* memory);
+void ArmCpuRun(ArmAllRegisters* registers, Memory* memory, uint32_t num_steps);
+
+#endif  // _WEBGBA_EMULATOR_CPU_ARM7TDMI_EXECUTE_
