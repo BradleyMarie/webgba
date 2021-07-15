e4f94ba0534aaec487b6f4d90d72fbf25a7a1c3e
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-15 10:18:29 -0700

    Add IRAM and WRAM

---
+++ emulator/memory/gba/iram/iram.h
@@ -0,0 +1,8 @@
+#ifndef _WEBGBA_EMULATOR_MEMORY_GBA_IRAM_
+#define _WEBGBA_EMULATOR_MEMORY_GBA_IRAM_
+
+#include "emulator/memory/memory.h"
+
+Memory* AllocateIRam();
+
+#endif  // _WEBGBA_EMULATOR_MEMORY_GBA_IRAM_
---
+++ emulator/memory/gba/wram/wram.h
@@ -0,0 +1,8 @@
+#ifndef _WEBGBA_EMULATOR_MEMORY_GBA_WRAM_
+#define _WEBGBA_EMULATOR_MEMORY_GBA_WRAM_
+
+#include "emulator/memory/memory.h"
+
+Memory* AllocateWRam();
+
+#endif  // _WEBGBA_EMULATOR_MEMORY_GBA_WRAM_
