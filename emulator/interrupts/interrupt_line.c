50acf736d3560b644c13995868b4f49e7e86bec0
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-19 10:20:19 -0700

    Add interrupt line interface

---
+++ emulator/interrupts/interrupt_line.c
@@ -0,0 +1,33 @@
+#include "emulator/interrupts/interrupt_line.h"
+
+#include <stdlib.h>
+
+struct _InterruptLine {
+  InterruptLineIsTriggeredFunction is_triggered;
+  InterruptLineContextFree free_context;
+  void *context;
+};
+
+InterruptLine *InterruptLineAllocate(
+    void *context, InterruptLineIsTriggeredFunction is_triggered,
+    InterruptLineContextFree free_context) {
+  InterruptLine *result = (InterruptLine *)malloc(sizeof(InterruptLine));
+  if (result == NULL) {
+    return NULL;
+  }
+
+  result->is_triggered = is_triggered;
+  result->free_context = free_context;
+  result->context = context;
+
+  return result;
+}
+
+bool InterruptLineIsTriggered(const InterruptLine *interrupt_line) {
+  return interrupt_line->is_triggered(interrupt_line->context);
+}
+
+void InterruptLineFree(InterruptLine *interrupt_line) {
+  interrupt_line->free_context(interrupt_line->context);
+  free(interrupt_line);
+}
