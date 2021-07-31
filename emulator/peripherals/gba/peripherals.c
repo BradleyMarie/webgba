211e19e72cb01aa521d139258c53e33b9c896642
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-31 00:09:57 -0700

    Add peripherals skeleton code

---
+++ emulator/peripherals/gba/peripherals.c
@@ -0,0 +1,9 @@
+#include "emulator/peripherals/gba/peripherals.h"
+
+bool GbaPeripheralsAllocate(GbaPeripherals **peripherals, Memory **registers) {
+  return false;
+}
+
+void GbaPeripheralsStep(GbaPeripherals *peripherals) {}
+
+void GbaPeripheralsFree(GbaPeripherals *peripherals) {}
