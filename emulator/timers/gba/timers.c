f8443ed0b97749d4f5b5881e34c00e46ac4efc4d
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-30 23:52:30 -0700

    Add timers skeleton code

---
+++ emulator/timers/gba/timers.c
@@ -0,0 +1,7 @@
+#include "emulator/timers/gba/timers.h"
+
+bool GbaTimersAllocate(GbaTimers **timers, Memory **registers) { return false; }
+
+void GbaTimersStep(GbaTimers *timers) {}
+
+void GbaTimersFree(GbaTimers *timers) {}
