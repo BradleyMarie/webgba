e5e3db117ca99e81ac04269f923627e5624d3158
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-07-30 23:49:32 -0700

    Add sound skeleton code

---
+++ emulator/sound/gba/sound.c
@@ -0,0 +1,7 @@
+#include "emulator/sound/gba/sound.h"
+
+bool GbaSpuAllocate(GbaSpu **spu, Memory **registers) { return false; }
+
+void GbaSpuStep(GbaSpu *ppu) {}
+
+void GbaSpuFree(GbaSpu *ppu) {}
