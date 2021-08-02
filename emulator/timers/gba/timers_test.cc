c91eb691599169509234ef6b0e375c05869d55ce
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-01 19:13:09 -0700

    Initialize counters when started

--- emulator/timers/gba/timers_test.cc
+++ emulator/timers/gba/timers_test.cc
@@ -40,48 +40,78 @@ class TimersTest : public testing::Test {
   Memory *regs_;
 };
 
-TEST_F(TimersTest, GbaSpuRegistersLoad32LE) {
+TEST_F(TimersTest, GbaSpuRegistersLoad32LENotStarted) {
   uint32_t contents;
   EXPECT_TRUE(Store32LE(regs_, TM0CNT_L_OFFSET, 0x11223344u));
   EXPECT_TRUE(Load32LE(regs_, TM0CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x11220000u, contents);
-  EXPECT_TRUE(Store32LE(regs_, TM1CNT_L_OFFSET, 0xAABBCCDDu));
+  EXPECT_TRUE(Store32LE(regs_, TM1CNT_L_OFFSET, 0x55667788u));
   EXPECT_TRUE(Load32LE(regs_, TM1CNT_L_OFFSET, &contents));
-  EXPECT_EQ(0xAABB0000u, contents);
+  EXPECT_EQ(0x55660000u, contents);
   EXPECT_TRUE(Store32LE(regs_, TM2CNT_L_OFFSET, 0x11223344u));
   EXPECT_TRUE(Load32LE(regs_, TM2CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x11220000u, contents);
-  EXPECT_TRUE(Store32LE(regs_, TM3CNT_L_OFFSET, 0xAABBCCDDu));
+  EXPECT_TRUE(Store32LE(regs_, TM3CNT_L_OFFSET, 0x55667788u));
   EXPECT_TRUE(Load32LE(regs_, TM3CNT_L_OFFSET, &contents));
-  EXPECT_EQ(0xAABB0000u, contents);
+  EXPECT_EQ(0x55660000u, contents);
+
+  EXPECT_FALSE(InterruptLineIsRaised(irq_));
 }
 
-TEST_F(TimersTest, GbaSpuRegistersLoad16LE) {
+TEST_F(TimersTest, GbaSpuRegistersLoad32LEStarted) {
+  uint32_t contents;
+  EXPECT_TRUE(Store32LE(regs_, TM0CNT_L_OFFSET, 0xAABB3344u));
+  EXPECT_TRUE(Load32LE(regs_, TM0CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0xAABB3344u, contents);
+  EXPECT_TRUE(Store32LE(regs_, TM1CNT_L_OFFSET, 0xAABB7788u));
+  EXPECT_TRUE(Load32LE(regs_, TM1CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0xAABB7788u, contents);
+  EXPECT_TRUE(Store32LE(regs_, TM2CNT_L_OFFSET, 0xAABB3344u));
+  EXPECT_TRUE(Load32LE(regs_, TM2CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0xAABB3344u, contents);
+  EXPECT_TRUE(Store32LE(regs_, TM3CNT_L_OFFSET, 0xAABB7788u));
+  EXPECT_TRUE(Load32LE(regs_, TM3CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0xAABB7788u, contents);
+
+  EXPECT_FALSE(InterruptLineIsRaised(irq_));
+}
+
+TEST_F(TimersTest, GbaSpuRegistersLoad16LENotStarted) {
   uint16_t contents;
   EXPECT_TRUE(Store16LE(regs_, TM0CNT_L_OFFSET, 0x1122u));
   EXPECT_TRUE(Load16LE(regs_, TM0CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
-  EXPECT_TRUE(Store16LE(regs_, TM0CNT_H_OFFSET, 0x3344u));
+  EXPECT_TRUE(Store16LE(regs_, TM0CNT_H_OFFSET, 0xAABBu));
   EXPECT_TRUE(Load16LE(regs_, TM0CNT_H_OFFSET, &contents));
-  EXPECT_EQ(0x3344u, contents);
+  EXPECT_EQ(0xAABBu, contents);
+  EXPECT_TRUE(Load16LE(regs_, TM0CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x1122u, contents);
   EXPECT_TRUE(Store16LE(regs_, TM1CNT_L_OFFSET, 0x1122u));
   EXPECT_TRUE(Load16LE(regs_, TM1CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
-  EXPECT_TRUE(Store16LE(regs_, TM1CNT_H_OFFSET, 0x3344u));
+  EXPECT_TRUE(Store16LE(regs_, TM1CNT_H_OFFSET, 0xAABBu));
   EXPECT_TRUE(Load16LE(regs_, TM1CNT_H_OFFSET, &contents));
-  EXPECT_EQ(0x3344u, contents);
+  EXPECT_EQ(0xAABBu, contents);
+  EXPECT_TRUE(Load16LE(regs_, TM1CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x1122u, contents);
   EXPECT_TRUE(Store16LE(regs_, TM2CNT_L_OFFSET, 0x1122u));
   EXPECT_TRUE(Load16LE(regs_, TM2CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
-  EXPECT_TRUE(Store16LE(regs_, TM2CNT_H_OFFSET, 0x3344u));
+  EXPECT_TRUE(Store16LE(regs_, TM2CNT_H_OFFSET, 0xAABBu));
   EXPECT_TRUE(Load16LE(regs_, TM2CNT_H_OFFSET, &contents));
-  EXPECT_EQ(0x3344u, contents);
+  EXPECT_EQ(0xAABBu, contents);
+  EXPECT_TRUE(Load16LE(regs_, TM2CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x1122u, contents);
   EXPECT_TRUE(Store16LE(regs_, TM3CNT_L_OFFSET, 0x1122u));
   EXPECT_TRUE(Load16LE(regs_, TM3CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
-  EXPECT_TRUE(Store16LE(regs_, TM3CNT_H_OFFSET, 0x3344u));
+  EXPECT_TRUE(Store16LE(regs_, TM3CNT_H_OFFSET, 0xAABBu));
   EXPECT_TRUE(Load16LE(regs_, TM3CNT_H_OFFSET, &contents));
-  EXPECT_EQ(0x3344u, contents);
+  EXPECT_EQ(0xAABBu, contents);
+  EXPECT_TRUE(Load16LE(regs_, TM3CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x1122u, contents);
+
+  EXPECT_FALSE(InterruptLineIsRaised(irq_));
 }
 
 TEST_F(TimersTest, GbaSpuRegistersLoad8) {
@@ -98,6 +128,13 @@ TEST_F(TimersTest, GbaSpuRegistersLoad8) {
   EXPECT_TRUE(Store8(regs_, TM0CNT_H_OFFSET + 1u, 0x44u));
   EXPECT_TRUE(Load8(regs_, TM0CNT_H_OFFSET + 1u, &contents));
   EXPECT_EQ(0x44u, contents);
+  EXPECT_TRUE(Store8(regs_, TM0CNT_H_OFFSET, 0xAAu));
+  EXPECT_TRUE(Load8(regs_, TM0CNT_H_OFFSET, &contents));
+  EXPECT_EQ(0xAAu, contents);
+  EXPECT_TRUE(Load8(regs_, TM0CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x11u, contents);
+  EXPECT_TRUE(Load8(regs_, TM0CNT_L_OFFSET + 1u, &contents));
+  EXPECT_EQ(0x22u, contents);
   EXPECT_TRUE(Store8(regs_, TM1CNT_L_OFFSET, 0x11u));
   EXPECT_TRUE(Load8(regs_, TM1CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
@@ -110,6 +147,13 @@ TEST_F(TimersTest, GbaSpuRegistersLoad8) {
   EXPECT_TRUE(Store8(regs_, TM1CNT_H_OFFSET + 1u, 0x44u));
   EXPECT_TRUE(Load8(regs_, TM1CNT_H_OFFSET + 1u, &contents));
   EXPECT_EQ(0x44u, contents);
+  EXPECT_TRUE(Store8(regs_, TM1CNT_H_OFFSET, 0xAAu));
+  EXPECT_TRUE(Load8(regs_, TM1CNT_H_OFFSET, &contents));
+  EXPECT_EQ(0xAAu, contents);
+  EXPECT_TRUE(Load8(regs_, TM1CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x11u, contents);
+  EXPECT_TRUE(Load8(regs_, TM1CNT_L_OFFSET + 1u, &contents));
+  EXPECT_EQ(0x22u, contents);
   EXPECT_TRUE(Store8(regs_, TM2CNT_L_OFFSET, 0x11u));
   EXPECT_TRUE(Load8(regs_, TM2CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
@@ -122,6 +166,13 @@ TEST_F(TimersTest, GbaSpuRegistersLoad8) {
   EXPECT_TRUE(Store8(regs_, TM2CNT_H_OFFSET + 1u, 0x44u));
   EXPECT_TRUE(Load8(regs_, TM2CNT_H_OFFSET + 1u, &contents));
   EXPECT_EQ(0x44u, contents);
+  EXPECT_TRUE(Store8(regs_, TM2CNT_H_OFFSET, 0xAAu));
+  EXPECT_TRUE(Load8(regs_, TM2CNT_H_OFFSET, &contents));
+  EXPECT_EQ(0xAAu, contents);
+  EXPECT_TRUE(Load8(regs_, TM2CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x11u, contents);
+  EXPECT_TRUE(Load8(regs_, TM2CNT_L_OFFSET + 1u, &contents));
+  EXPECT_EQ(0x22u, contents);
   EXPECT_TRUE(Store8(regs_, TM3CNT_L_OFFSET, 0x11u));
   EXPECT_TRUE(Load8(regs_, TM3CNT_L_OFFSET, &contents));
   EXPECT_EQ(0x0u, contents);
@@ -134,6 +185,15 @@ TEST_F(TimersTest, GbaSpuRegistersLoad8) {
   EXPECT_TRUE(Store8(regs_, TM3CNT_H_OFFSET + 1u, 0x44u));
   EXPECT_TRUE(Load8(regs_, TM3CNT_H_OFFSET + 1u, &contents));
   EXPECT_EQ(0x44u, contents);
+  EXPECT_TRUE(Store8(regs_, TM3CNT_H_OFFSET, 0xAAu));
+  EXPECT_TRUE(Load8(regs_, TM3CNT_H_OFFSET, &contents));
+  EXPECT_EQ(0xAAu, contents);
+  EXPECT_TRUE(Load8(regs_, TM3CNT_L_OFFSET, &contents));
+  EXPECT_EQ(0x11u, contents);
+  EXPECT_TRUE(Load8(regs_, TM3CNT_L_OFFSET + 1u, &contents));
+  EXPECT_EQ(0x22u, contents);
+
+  EXPECT_FALSE(InterruptLineIsRaised(irq_));
 }
 
 TEST_F(TimersTest, OutOfBounds) {
@@ -143,4 +203,6 @@ TEST_F(TimersTest, OutOfBounds) {
   EXPECT_FALSE(Load32LE(regs_, TM3CNT_H_OFFSET + 2u, nullptr));
   EXPECT_FALSE(Load16LE(regs_, TM3CNT_H_OFFSET + 2u, nullptr));
   EXPECT_FALSE(Load8(regs_, TM3CNT_H_OFFSET + 2u, nullptr));
+
+  EXPECT_FALSE(InterruptLineIsRaised(irq_));
 }
