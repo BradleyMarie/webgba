db0130b1dc79dfb629b183a0b491fe8c70073afe
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-08 18:35:31 -0700

    Complete DMA unit implementation

--- emulator/dma/gba/dma.c
+++ emulator/dma/gba/dma.c
@@ -23,6 +23,11 @@
 #define GBA_DMA_UNIT_REGISTERS_SIZE 0x30u
 #define GBA_NUM_DMA_UNITS 4u
 
+#define GBA_DMA_ADDR_INCREMENT 0u
+#define GBA_DMA_ADDR_DECREMENT 1u
+#define GBA_DMA_ADDR_FIXED 2u
+#define GBA_DMA_ADDR_INCREMENT_RELOAD 3u
+
 #define GBA_DMA_IMMEDIATE 0u
 #define GBA_DMA_VBLANK 1u
 #define GBA_DMA_HBLANK 2u
@@ -35,7 +40,7 @@ typedef union {
     unsigned short src_addr : 2;
     bool repeat : 1;
     bool transfer_word : 1;
-    bool gamepak_drq : 1;
+    bool gamepak_drq : 1;  // Not Implemented
     unsigned short start_timing : 2;
     bool irq_enable : 1;
     bool enabled : 1;
@@ -59,6 +64,10 @@ typedef union {
 
 struct _GbaDmaUnit {
   unsigned active;
+  bool enabled[GBA_NUM_DMA_UNITS];
+  uint32_t current_source[GBA_NUM_DMA_UNITS];
+  uint32_t current_destination[GBA_NUM_DMA_UNITS];
+  uint16_t transfers_remaining[GBA_NUM_DMA_UNITS];
   GbaDmaUnitRegisters registers;
   GbaPlatform *platform;
   uint16_t reference_count;
@@ -71,19 +80,56 @@ static void GbaDmaUnitClearActive(GbaDmaUnit *dma_unit, uint_fast8_t index) {
 }
 
 static void GbaDmaUnitSetActiveBitTo(GbaDmaUnit *dma_unit, uint_fast8_t index,
-                                     bool value) {
+                                     bool active) {
   assert(index < GBA_NUM_DMA_UNITS);
   GbaDmaUnitClearActive(dma_unit, index);
-  dma_unit->active |= (unsigned)value << index;
+  dma_unit->active |= (unsigned)active << index;
+}
+
+static bool GbaDmaUnitIsActiveByIndex(const GbaDmaUnit *dma_unit,
+                                      uint_fast8_t index) {
+  unsigned bit_to_check = 1u << index;
+  return dma_unit->active & bit_to_check;
+}
+
+static void GbaDmaUnitReloadSource(GbaDmaUnit *dma_unit, uint_fast8_t index) {
+  assert(index < GBA_NUM_DMA_UNITS);
+  static const uint32_t address_mask[GBA_NUM_DMA_UNITS] = {
+      0x07FFFFFFu, 0x0FFFFFFFu, 0x0FFFFFFFu, 0x0FFFFFFFu};
+  dma_unit->current_source[index] =
+      dma_unit->registers.units[index].source & address_mask[index];
+}
+
+static void GbaDmaUnitReloadDestination(GbaDmaUnit *dma_unit,
+                                        uint_fast8_t index) {
+  assert(index < GBA_NUM_DMA_UNITS);
+  static const uint32_t address_mask[GBA_NUM_DMA_UNITS] = {
+      0x07FFFFFFu, 0x0FFFFFFFu, 0x0FFFFFFFu, 0x0FFFFFFFu};
+  dma_unit->current_destination[index] =
+      dma_unit->registers.units[index].destination & address_mask[index];
+}
+
+static void GbaDmaUnitReloadTransferSize(GbaDmaUnit *dma_unit,
+                                         uint_fast8_t index) {
+  assert(index < GBA_NUM_DMA_UNITS);
+  static const uint16_t transfer_mask[GBA_NUM_DMA_UNITS] = {0x3FFFu, 0x3FFFu,
+                                                            0x3FFFu, 0xFFFFu};
+  dma_unit->transfers_remaining[index] =
+      dma_unit->registers.units[index].word_count & transfer_mask[index];
 }
 
 static void GbaDmaUnitUpdateAfterWrite(GbaDmaUnit *dma_unit) {
-  assert(!dma_unit->active);
   for (uint_fast8_t i = 0; i < GBA_NUM_DMA_UNITS; i++) {
     bool active =
         dma_unit->registers.units[i].control.enabled &&
         dma_unit->registers.units[i].control.start_timing == GBA_DMA_IMMEDIATE;
     GbaDmaUnitSetActiveBitTo(dma_unit, i, active);
+    if (dma_unit->registers.units[i].control.enabled && !dma_unit->enabled[i]) {
+      GbaDmaUnitReloadSource(dma_unit, i);
+      GbaDmaUnitReloadDestination(dma_unit, i);
+      GbaDmaUnitReloadTransferSize(dma_unit, i);
+    }
+    dma_unit->enabled[i] = dma_unit->registers.units[i].control.enabled;
   }
 }
 
@@ -231,9 +277,67 @@ bool GbaDmaUnitAllocate(GbaPlatform *platform, GbaDmaUnit **dma_unit,
   return true;
 }
 
-bool GbaDmaIsActive(const GbaDmaUnit *dma_unit) { return dma_unit->active; }
+bool GbaDmaUnitIsActive(const GbaDmaUnit *dma_unit) { return dma_unit->active; }
 
-void GbaDmaUnitStep(GbaDmaUnit *dma_unit, Memory *memory) {}
+void GbaDmaUnitStep(GbaDmaUnit *dma_unit, Memory *memory) {
+  assert(GbaDmaUnitIsActive(dma_unit));
+  for (uint_fast8_t i = 0; i < GBA_NUM_DMA_UNITS; i++) {
+    if (!GbaDmaUnitIsActiveByIndex(dma_unit, i)) {
+      continue;
+    }
+
+    uint32_t transfer_size;
+    if (dma_unit->registers.units[i].control.transfer_word) {
+      const static uint32_t address_mask = 0xFFFFFFFCu;
+      uint32_t value;
+      Load32LE(memory, dma_unit->current_source[i] & address_mask, &value);
+      Store32LE(memory, dma_unit->current_destination[i] & address_mask, value);
+      transfer_size = 4u;
+    } else {
+      const static uint32_t address_mask = 0xFFFFFFFEu;
+      uint16_t value;
+      Load16LE(memory, dma_unit->current_source[i] & address_mask, &value);
+      Store16LE(memory, dma_unit->current_destination[i] & address_mask, value);
+      transfer_size = 2u;
+    }
+
+    switch (dma_unit->registers.units[i].control.src_addr) {
+      case GBA_DMA_ADDR_INCREMENT:
+        dma_unit->current_source[i] += transfer_size;
+        break;
+      case GBA_DMA_ADDR_DECREMENT:
+        dma_unit->current_source[i] -= transfer_size;
+        break;
+    }
+
+    switch (dma_unit->registers.units[i].control.dest_addr) {
+      case GBA_DMA_ADDR_INCREMENT:
+        dma_unit->current_destination[i] += transfer_size;
+        break;
+      case GBA_DMA_ADDR_DECREMENT:
+        dma_unit->current_destination[i] -= transfer_size;
+        break;
+      case GBA_DMA_ADDR_INCREMENT_RELOAD:
+        dma_unit->current_destination[i] += transfer_size;
+        break;
+    }
+
+    dma_unit->transfers_remaining[i] -= 1u;
+    if (dma_unit->transfers_remaining[i] == 0u) {
+      GbaDmaUnitClearActive(dma_unit, i);
+      if (!dma_unit->registers.units[i].control.repeat) {
+        dma_unit->registers.units[i].control.enabled = false;
+      } else {
+        if (dma_unit->registers.units[i].control.dest_addr ==
+            GBA_DMA_ADDR_INCREMENT_RELOAD) {
+          GbaDmaUnitReloadDestination(dma_unit, i);
+        }
+        GbaDmaUnitReloadTransferSize(dma_unit, i);
+      }
+    }
+    break;
+  }
+}
 
 void GbaDmaUnitSignalHBlank(GbaDmaUnit *dma_unit, uint_fast8_t vcount) {
   for (uint_fast8_t i = 0; i < GBA_NUM_DMA_UNITS; i++) {
