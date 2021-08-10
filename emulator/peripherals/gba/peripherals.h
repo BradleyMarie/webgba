005de3a2a40cc04febc1a1edf92f870eb400d183
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-09 23:22:14 -0700

    Expose gamepad from GBA peripherals

--- emulator/peripherals/gba/peripherals.h
+++ emulator/peripherals/gba/peripherals.h
@@ -2,12 +2,13 @@
 #define _WEBGBA_EMULATOR_TIMERS_GBA_TIMERS_
 
 #include "emulator/memory/memory.h"
+#include "emulator/peripherals/gamepad.h"
 #include "emulator/platform/gba/platform.h"
 
 typedef struct _GbaPeripherals GbaPeripherals;
 
 bool GbaPeripheralsAllocate(GbaPlatform *platform, GbaPeripherals **peripherals,
-                            Memory **registers);
+                            GamePad **gamepad, Memory **registers);
 
 void GbaPeripheralsStep(GbaPeripherals *peripherals);
 
