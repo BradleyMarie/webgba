71d8026f5192772610a07795b6357ef74de08745
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-11 21:58:05 -0700

    Mirror WRAM across bank

--- emulator/memory/gba/wram/wram_test.cc
+++ emulator/memory/gba/wram/wram_test.cc
@@ -20,33 +20,28 @@ class WRamTest : public testing::Test {
 };
 
 TEST_F(WRamTest, LoadStore8Succeeds) {
-  EXPECT_TRUE(Store8(memory_, WRAM_SIZE - 1u, 0x20u));
+  EXPECT_TRUE(Store8(memory_, 0x0u, 0x20u));
   uint8_t value;
-  EXPECT_TRUE(Load8(memory_, WRAM_SIZE - 1u, &value));
+  EXPECT_TRUE(Load8(memory_, 0x0u, &value));
+  EXPECT_EQ(0x20u, value);
+  EXPECT_TRUE(Load8(memory_, WRAM_SIZE, &value));
   EXPECT_EQ(0x20u, value);
 }
 
-TEST_F(WRamTest, LoadStore8Fails) {
-  EXPECT_FALSE(Store8(memory_, WRAM_SIZE, 0x20u));
-  uint8_t value;
-  EXPECT_FALSE(Load8(memory_, WRAM_SIZE, &value));
-}
-
 TEST_F(WRamTest, LoadStore16Succeeds) {
-  EXPECT_TRUE(Store16LE(memory_, WRAM_SIZE - 2u, 0x2030u));
+  EXPECT_TRUE(Store16LE(memory_, 0x0u, 0x2030u));
   uint16_t value;
-  EXPECT_TRUE(Load16LE(memory_, WRAM_SIZE - 2u, &value));
+  EXPECT_TRUE(Load16LE(memory_, 0x0u, &value));
+  EXPECT_EQ(0x2030u, value);
+  EXPECT_TRUE(Load16LE(memory_, WRAM_SIZE, &value));
   EXPECT_EQ(0x2030u, value);
 }
 
-TEST_F(WRamTest, LoadStore16Fails) {
-  EXPECT_FALSE(Store16LE(memory_, WRAM_SIZE - 1u, 0x2030u));
-  uint16_t value;
-  EXPECT_FALSE(Load16LE(memory_, WRAM_SIZE - 1u, &value));
-}
-
-TEST_F(WRamTest, LoadStore32Fails) {
-  EXPECT_FALSE(Store32LE(memory_, 0x10u, 0x20304050u));
+TEST_F(WRamTest, LoadStore32Succeeds) {
+  EXPECT_TRUE(Store32LE(memory_, 0x0u, 0x20304050u));
   uint32_t value;
-  EXPECT_FALSE(Load32LE(memory_, 0x10u, &value));
+  EXPECT_TRUE(Load32LE(memory_, 0x0u, &value));
+  EXPECT_EQ(0x20304050u, value);
+  EXPECT_TRUE(Load32LE(memory_, WRAM_SIZE, &value));
+  EXPECT_EQ(0x20304050u, value);
 }
