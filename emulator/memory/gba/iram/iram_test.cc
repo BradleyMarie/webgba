d847990ba656901fdca5b4e8aeb0bf0952a25847
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-11 21:51:53 -0700

    Mirror IRAM across whole bank

--- emulator/memory/gba/iram/iram_test.cc
+++ emulator/memory/gba/iram/iram_test.cc
@@ -20,40 +20,28 @@ class IRamTest : public testing::Test {
 };
 
 TEST_F(IRamTest, LoadStore8Succeeds) {
-  EXPECT_TRUE(Store8(memory_, IRAM_SIZE - 1u, 0x20u));
+  EXPECT_TRUE(Store8(memory_, 0x0u, 0x20u));
   uint8_t value;
-  EXPECT_TRUE(Load8(memory_, IRAM_SIZE - 1u, &value));
+  EXPECT_TRUE(Load8(memory_, 0x0u, &value));
+  EXPECT_EQ(0x20u, value);
+  EXPECT_TRUE(Load8(memory_, IRAM_SIZE, &value));
   EXPECT_EQ(0x20u, value);
 }
 
-TEST_F(IRamTest, LoadStore8Fails) {
-  EXPECT_FALSE(Store8(memory_, IRAM_SIZE, 0x20u));
-  uint8_t value;
-  EXPECT_FALSE(Load8(memory_, IRAM_SIZE, &value));
-}
-
 TEST_F(IRamTest, LoadStore16Succeeds) {
-  EXPECT_TRUE(Store16LE(memory_, IRAM_SIZE - 2u, 0x2030u));
+  EXPECT_TRUE(Store16LE(memory_, 0x0u, 0x2030u));
   uint16_t value;
-  EXPECT_TRUE(Load16LE(memory_, IRAM_SIZE - 2u, &value));
+  EXPECT_TRUE(Load16LE(memory_, 0x0u, &value));
+  EXPECT_EQ(0x2030u, value);
+  EXPECT_TRUE(Load16LE(memory_, IRAM_SIZE, &value));
   EXPECT_EQ(0x2030u, value);
 }
 
-TEST_F(IRamTest, LoadStore16Fails) {
-  EXPECT_FALSE(Store16LE(memory_, IRAM_SIZE - 1u, 0x2030u));
-  uint16_t value;
-  EXPECT_FALSE(Load16LE(memory_, IRAM_SIZE - 1u, &value));
-}
-
 TEST_F(IRamTest, LoadStore32Succeeds) {
-  EXPECT_TRUE(Store32LE(memory_, IRAM_SIZE - 4u, 0x20304050u));
+  EXPECT_TRUE(Store32LE(memory_, 0x0u, 0x20304050u));
   uint32_t value;
-  EXPECT_TRUE(Load32LE(memory_, IRAM_SIZE - 4u, &value));
+  EXPECT_TRUE(Load32LE(memory_, 0x0u, &value));
   EXPECT_EQ(0x20304050u, value);
-}
-
-TEST_F(IRamTest, LoadStore32Fails) {
-  EXPECT_FALSE(Store32LE(memory_, IRAM_SIZE - 3u, 0x20304050u));
-  uint32_t value;
-  EXPECT_FALSE(Load32LE(memory_, IRAM_SIZE - 3u, &value));
+  EXPECT_TRUE(Load32LE(memory_, IRAM_SIZE, &value));
+  EXPECT_EQ(0x20304050u, value);
 }
