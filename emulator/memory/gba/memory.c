53261ed30b3e286c7bdae0a104ea2fd8e7246152
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-13 23:53:29 -0700

    Allocate IO bank from GBA Memory

--- emulator/memory/gba/memory.c
+++ emulator/memory/gba/memory.c
@@ -4,6 +4,7 @@
 
 #include "emulator/memory/gba/bad/bad.h"
 #include "emulator/memory/gba/bios/bios.h"
+#include "emulator/memory/gba/io/io.h"
 #include "emulator/memory/gba/iram/iram.h"
 #include "emulator/memory/gba/wram/wram.h"
 
@@ -113,12 +114,17 @@ static void GbaMemoryFree(void* context) {
   MemoryFree(gba_memory->vram);
   MemoryFree(gba_memory->oam);
   MemoryFree(gba_memory->game);
+  MemoryFree(gba_memory->sram);
   MemoryFree(gba_memory->bad);
   free(gba_memory);
 }
 
-Memory* GbaMemoryAllocate(Memory* io, Memory* palette, Memory* vram,
-                          Memory* oam, Memory* game, Memory* sram) {
+Memory* GbaMemoryAllocate(Memory* ppu_registers, Memory* sound_registers,
+                          Memory* dma_registers, Memory* timer_registers,
+                          Memory* peripheral_registers,
+                          Memory* platform_registers, Memory* palette,
+                          Memory* vram, Memory* oam, Memory* game,
+                          Memory* sram) {
   GbaMemory* gba_memory = (GbaMemory*)malloc(sizeof(GbaMemory));
   if (gba_memory == NULL) {
     return NULL;
@@ -158,7 +164,7 @@ Memory* GbaMemoryAllocate(Memory* io, Memory* palette, Memory* vram,
   gba_memory->banks[0x1u] = bad;
   gba_memory->banks[0x2u] = ewram;
   gba_memory->banks[0x3u] = iwram;
-  gba_memory->banks[0x4u] = io;
+  gba_memory->banks[0x4u] = NULL;
   gba_memory->banks[0x5u] = palette;
   gba_memory->banks[0x6u] = vram;
   gba_memory->banks[0x7u] = oam;
@@ -174,11 +180,12 @@ Memory* GbaMemoryAllocate(Memory* io, Memory* palette, Memory* vram,
   gba_memory->bios = bios;
   gba_memory->ewram = ewram;
   gba_memory->iwram = iwram;
-  gba_memory->io = io;
-  gba_memory->palette = palette;
-  gba_memory->vram = vram;
-  gba_memory->oam = oam;
-  gba_memory->game = game;
+  gba_memory->io = NULL;
+  gba_memory->palette = NULL;
+  gba_memory->vram = NULL;
+  gba_memory->oam = NULL;
+  gba_memory->game = NULL;
+  gba_memory->sram = NULL;
   gba_memory->bad = bad;
 
   Memory* result = MemoryAllocate(
@@ -192,6 +199,27 @@ Memory* GbaMemoryAllocate(Memory* io, Memory* palette, Memory* vram,
     free(gba_memory);
     return NULL;
   }
+
+  // This is a bit of a hack in my opinion. It'd be a little cleaner if
+  // gba_memory was not modified by this function after allocating the result.
+  // That said, this is easier than the alternative mechanisms for *not* freeing
+  // the inputs to this function in the presence of a failed allocation.
+  Memory* io = IoMemoryAllocate(ppu_registers, sound_registers, dma_registers,
+                                timer_registers, peripheral_registers,
+                                platform_registers);
+  if (io == NULL) {
+    MemoryFree(result);
+    return NULL;
+  }
+
+  gba_memory->banks[0x4u] = io;
+
+  gba_memory->io = io;
+  gba_memory->palette = palette;
+  gba_memory->vram = vram;
+  gba_memory->oam = oam;
+  gba_memory->game = game;
+  gba_memory->sram = sram;
 
   return result;
 }
--- emulator/memory/gba/memory.h
+++ emulator/memory/gba/memory.h
@@ -3,7 +3,11 @@
 
 #include "emulator/memory/memory.h"
 
-Memory* GbaMemoryAllocate(Memory* io, Memory* palette, Memory* vram,
-                          Memory* oam, Memory* game, Memory* sram);
+Memory* GbaMemoryAllocate(Memory* ppu_registers, Memory* sound_registers,
+                          Memory* dma_registers, Memory* timer_registers,
+                          Memory* peripheral_registers,
+                          Memory* platform_registers, Memory* palette,
+                          Memory* vram, Memory* oam, Memory* game,
+                          Memory* sram);
 
 #endif  // _WEBGBA_EMULATOR_MEMORY_GBA_MEMORY_
