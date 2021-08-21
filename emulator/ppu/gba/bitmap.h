80bc5e6a6b306259c7dbf077530b22223ff5e992
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-08-20 21:23:05 -0700

    Unify screen logic

--- emulator/ppu/gba/bitmap.h
+++ emulator/ppu/gba/bitmap.h
@@ -1,24 +1,25 @@
 #ifndef _WEBGBA_EMULATOR_PPU_GBA_BITMAP_
 #define _WEBGBA_EMULATOR_PPU_GBA_BITMAP_
 
+#include "emulator/ppu/gba/screen.h"
 #include "emulator/ppu/gba/types.h"
 
 void GbaPpuRenderMode3Pixel(const GbaPpuMemory* memory,
                             const GbaPpuRegisters* registers,
                             GbaPpuInternalRegisters* internal_registers,
                             uint_fast8_t x, uint_fast8_t y,
-                            GbaPpuFrameBuffer* framebuffer);
+                            GbaPpuScreen* screen);
 
 void GbaPpuRenderMode4Pixel(const GbaPpuMemory* memory,
                             const GbaPpuRegisters* registers,
                             GbaPpuInternalRegisters* internal_registers,
                             uint_fast8_t x, uint_fast8_t y,
-                            GbaPpuFrameBuffer* framebuffer);
+                            GbaPpuScreen* screen);
 
 void GbaPpuRenderMode5Pixel(const GbaPpuMemory* memory,
                             const GbaPpuRegisters* registers,
                             GbaPpuInternalRegisters* internal_registers,
                             uint_fast8_t x, uint_fast8_t y,
-                            GbaPpuFrameBuffer* framebuffer);
+                            GbaPpuScreen* screen);
 
 #endif  // _WEBGBA_EMULATOR_PPU_GBA_BITMAP_
