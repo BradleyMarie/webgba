7377fbc253780d74c1567bb34e774bf0aa97cbb0
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-09-06 22:45:44 -0700

    Update object state on OAM accesses

--- emulator/ppu/gba/oam/oam.h
+++ emulator/ppu/gba/oam/oam.h
@@ -3,8 +3,10 @@
 
 #include "emulator/memory/memory.h"
 #include "emulator/ppu/gba/memory.h"
+#include "emulator/ppu/gba/object_state.h"
 
 Memory* OamAllocate(GbaPpuObjectAttributeMemory* oam_memory,
+                    GbaPpuObjectState* object_state,
                     MemoryContextFree free_routine, void* free_address);
 
 #endif  // _WEBGBA_EMULATOR_PPU_GBA_OAM_OAM_
