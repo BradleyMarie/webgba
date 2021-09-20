5299f8a77e007d12c58fa5cdf15823e5eff667de
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-09-20 10:01:45 -0700

    Remove branch from game pak memory and always allocate 32MB

--- emulator/game/gba/game.c
+++ emulator/game/gba/game.c
@@ -4,51 +4,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  uint32_t size;
-  unsigned char bytes[];
+#define GBA_GAME_MAX_SIZE 0x1000000u  // 32MB
+
+typedef union {
+  uint8_t bytes[GBA_GAME_MAX_SIZE];
 } GamePak;
 
 static bool GbaGameLoad32LE(const void *context, uint32_t address,
                             uint32_t *value) {
   assert((address & 0x3u) == 0u);
+  assert(address + 4u <= GBA_GAME_MAX_SIZE);
 
   const GamePak *gamepak = (const GamePak *)context;
-
-  if (address + 4u <= gamepak->size) {
-    return false;
-  }
-
   const unsigned char *first_byte = gamepak->bytes + address;
   *value = *(const uint32_t *)(const void *)first_byte;
+
   return true;
 }
 
 static bool GbaGameLoad16LE(const void *context, uint32_t address,
                             uint16_t *value) {
   assert((address & 0x1u) == 0u);
+  assert(address + 2u <= GBA_GAME_MAX_SIZE);
 
   const GamePak *gamepak = (const GamePak *)context;
-
-  if (address + 2u <= gamepak->size) {
-    return false;
-  }
-
   const unsigned char *first_byte = gamepak->bytes + address;
   *value = *(const uint16_t *)(const void *)first_byte;
+
   return true;
 }
 
 static bool GbaGameLoad8(const void *context, uint32_t address,
                          uint8_t *value) {
+  assert(address < GBA_GAME_MAX_SIZE);
+
   const GamePak *gamepak = (const GamePak *)context;
+  *value = gamepak->bytes[address];
 
-  if (address <= gamepak->size) {
-    return false;
-  }
-
-  const unsigned char *first_byte = gamepak->bytes + address;
-  *value = *(const uint8_t *)(const void *)first_byte;
   return true;
 }
 
@@ -71,22 +63,11 @@ void GbaGameFree(void *context) {
 
 bool GbaGameLoad(const char *rom_data, uint32_t rom_size,
                  SaveStorageType *save_storage_type, Memory **game_rom) {
-  if (rom_size > 0x1000000u) {
-    // Roms larger than 32MB are not supported
+  if (rom_size > GBA_GAME_MAX_SIZE) {
     return false;
   }
 
-  // Round memory size up to nearest power of two
-  uint32_t memory_size = rom_size;
-  memory_size = memory_size - 1;
-  memory_size |= memory_size >> 1;
-  memory_size |= memory_size >> 2;
-  memory_size |= memory_size >> 4;
-  memory_size |= memory_size >> 8;
-  memory_size |= memory_size >> 16;
-  memory_size += 1;
-
-  GamePak *gamepak = (GamePak *)calloc(1, memory_size + sizeof(uint32_t));
+  GamePak *gamepak = (GamePak *)calloc(1, sizeof(GamePak));
   if (gamepak == NULL) {
     return false;
   }
