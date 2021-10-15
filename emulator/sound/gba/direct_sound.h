1f6e5809b0281441bb5b227a5553d68d274391b0
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-10-14 23:48:52 -0700

    Add DirecSoundChannelClear function

--- emulator/sound/gba/direct_sound.h
+++ emulator/sound/gba/direct_sound.h
@@ -17,6 +17,8 @@ uint32_t DirectSoundChannelPeekBack(DirectSoundChannel* channel);
 
 bool DirectSoundChannelPop(DirectSoundChannel* channel, int16_t* value);
 
+void DirectSoundChannelClear(DirectSoundChannel* channel);
+
 void DirectSoundChannelFree(DirectSoundChannel* channel);
 
 #endif  // _WEBGBA_EMULATOR_SOUND_GBA_DIRECT_SOUND_
