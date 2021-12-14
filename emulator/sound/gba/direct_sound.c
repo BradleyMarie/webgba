c78a0bf3d0795ed5d59adf5b2177ca1e526ba50f
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-12-13 18:34:04 -0800

    Reimplement direct sound buffer

--- emulator/sound/gba/direct_sound.c
+++ emulator/sound/gba/direct_sound.c
@@ -1,116 +1,49 @@
 #include "emulator/sound/gba/direct_sound.h"
 
-#include <stdlib.h>
+#define LOW_WATER_MARK 16
 
-#define FIFO_BUFFER_SIZE_PACKED_SAMPLES 7u
-#define FIFO_BUFFER_DMA_THRESHOLD 3u
-
-struct _DirectSoundChannel {
-  uint32_t packed_samples_high[FIFO_BUFFER_SIZE_PACKED_SAMPLES];
-  uint32_t packed_samples_low[FIFO_BUFFER_SIZE_PACKED_SAMPLES];
-  uint_fast8_t sample_index;
-  uint_fast8_t front;
-  uint_fast8_t size;
-  bool write_upper_half;
-};
-
-DirectSoundChannel* DirectSoundChannelAllocate() {
-  DirectSoundChannel* result = calloc(1u, sizeof(DirectSoundChannel));
-  return result;
-}
-
-void DirectSoundChannelPush8BitSamples(DirectSoundChannel* channel,
-                                       uint32_t packed_samples) {
-  uint_fast8_t next =
-      (channel->front + channel->size) % FIFO_BUFFER_SIZE_PACKED_SAMPLES;
-
-  channel->packed_samples_high[next] = packed_samples;
-  channel->packed_samples_low[next] = 0u;
-
-  if (channel->size < FIFO_BUFFER_SIZE_PACKED_SAMPLES) {
-    channel->size += 1u;
-  }
-
-  channel->write_upper_half = false;
-}
-
-void DirectSoundChannelPush16BitSamples(DirectSoundChannel* channel,
-                                        uint32_t packed_samples) {
-  uint_fast8_t next =
-      (channel->front + channel->size) % FIFO_BUFFER_SIZE_PACKED_SAMPLES;
-
-  uint32_t high_value_to_write = channel->packed_samples_high[next];
-  uint32_t low_value_to_write = channel->packed_samples_low[next];
-
-  uint16_t first_sample = packed_samples;
-  uint16_t second_sample = packed_samples >> 16u;
-
-  uint16_t high_samples = (first_sample >> 8u) | (second_sample & 0xFF00u);
-  uint16_t low_samples = (first_sample & 0xFFu) | (second_sample << 8u);
-
-  if (channel->write_upper_half) {
-    high_value_to_write &= 0xFFFF0000u;
-    high_value_to_write |= high_samples;
-
-    low_value_to_write &= 0xFFFF0000u;
-    low_value_to_write |= low_samples;
-
-    channel->write_upper_half = false;
+void DirectSoundChannelPush(DirectSoundChannel* channel, uint8_t sample) {
+  if (channel->num_samples == DIRECT_SOUND_CHANNEL_BUFFER_SIZE) {
+    channel->samples[channel->front_index % DIRECT_SOUND_CHANNEL_BUFFER_SIZE] =
+        (int8_t)sample;
+    channel->front_index += 1u;
   } else {
-    high_value_to_write &= 0x0000FFFFu;
-    high_value_to_write |= (uint32_t)high_samples << 16u;
-
-    low_value_to_write &= 0x0000FFFFu;
-    low_value_to_write |= (uint32_t)low_samples << 16u;
-
-    channel->write_upper_half = true;
-  }
-
-  channel->packed_samples_high[next] = high_value_to_write;
-  channel->packed_samples_low[next] = low_value_to_write;
-
-  if (channel->write_upper_half &&
-      channel->size < FIFO_BUFFER_SIZE_PACKED_SAMPLES) {
-    channel->size += 1u;
+    uint_fast8_t insertion_index =
+        (channel->front_index + channel->num_samples) %
+        DIRECT_SOUND_CHANNEL_BUFFER_SIZE;
+    channel->samples[insertion_index] = (int8_t)sample;
+    channel->num_samples += 1u;
   }
 }
 
-bool DirectSoundChannelPop(DirectSoundChannel* channel, int16_t* value) {
-  if (channel->size == 0u) {
+void DirectSoundChannelPushTwo(DirectSoundChannel* channel,
+                               uint16_t packed_samples) {
+  DirectSoundChannelPush(channel, (uint8_t)packed_samples);
+  DirectSoundChannelPush(channel, (uint8_t)(packed_samples >> 8u));
+}
+
+void DirectSoundChannelPushFour(DirectSoundChannel* channel,
+                                uint32_t packed_samples) {
+  DirectSoundChannelPushTwo(channel, (uint16_t)packed_samples);
+  DirectSoundChannelPushTwo(channel, (uint16_t)(packed_samples >> 16u));
+}
+
+bool DirectSoundChannelPop(DirectSoundChannel* channel, int8_t* value) {
+  if (channel->num_samples == 0) {
     *value = 0;
     return true;
   }
 
-  uint_fast8_t bit_shift = 8u * channel->sample_index;
-  uint8_t low_bits = channel->packed_samples_low[channel->front] >> bit_shift;
-  uint8_t high_bits = channel->packed_samples_high[channel->front] >> bit_shift;
+  *value =
+      channel->samples[channel->front_index % DIRECT_SOUND_CHANNEL_BUFFER_SIZE];
 
-  *value = (uint16_t)low_bits | (((uint16_t)high_bits) << 8u);
+  channel->front_index += 1u;
+  channel->num_samples -= 1u;
 
-  channel->sample_index += 1u;
-  if (channel->sample_index == sizeof(uint32_t)) {
-    channel->sample_index = 0u;
-    channel->front += 1u;
-    channel->size -= 1u;
-
-    if (channel->front == FIFO_BUFFER_SIZE_PACKED_SAMPLES) {
-      channel->front = 0u;
-    }
-  }
-
-  return channel->size <= FIFO_BUFFER_DMA_THRESHOLD;
-}
-
-uint32_t DirectSoundChannelPeekBack(DirectSoundChannel* channel) {
-  uint_fast8_t next =
-      (channel->front + channel->size) % FIFO_BUFFER_SIZE_PACKED_SAMPLES;
-
-  return channel->packed_samples_high[next];
+  return channel->num_samples <= LOW_WATER_MARK;
 }
 
 void DirectSoundChannelClear(DirectSoundChannel* channel) {
-  channel->size = 0u;
-  channel->sample_index = 0u;
+  channel->front_index = 0u;
+  channel->num_samples = 0u;
 }
-
-void DirectSoundChannelFree(DirectSoundChannel* channel) { free(channel); }
--- emulator/sound/gba/direct_sound.h
+++ emulator/sound/gba/direct_sound.h
@@ -4,22 +4,24 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct _DirectSoundChannel DirectSoundChannel;
+#define DIRECT_SOUND_CHANNEL_BUFFER_SIZE 64
 
-DirectSoundChannel* DirectSoundChannelAllocate();
+typedef struct _DirectSoundChannel {
+  int8_t samples[DIRECT_SOUND_CHANNEL_BUFFER_SIZE];
+  uint_fast8_t front_index;
+  uint_fast8_t num_samples;
+} DirectSoundChannel;
 
-void DirectSoundChannelPush8BitSamples(DirectSoundChannel* channel,
-                                       uint32_t packed_samples);
+void DirectSoundChannelPush(DirectSoundChannel* channel, uint8_t sample);
 
-void DirectSoundChannelPush16BitSamples(DirectSoundChannel* channel,
-                                        uint32_t packed_samples);
+void DirectSoundChannelPushTwo(DirectSoundChannel* channel,
+                               uint16_t packed_samples);
 
-uint32_t DirectSoundChannelPeekBack(DirectSoundChannel* channel);
+void DirectSoundChannelPushFour(DirectSoundChannel* channel,
+                                uint32_t packed_samples);
 
-bool DirectSoundChannelPop(DirectSoundChannel* channel, int16_t* value);
+bool DirectSoundChannelPop(DirectSoundChannel* channel, int8_t* value);
 
 void DirectSoundChannelClear(DirectSoundChannel* channel);
-
-void DirectSoundChannelFree(DirectSoundChannel* channel);
 
 #endif  // _WEBGBA_EMULATOR_SOUND_GBA_DIRECT_SOUND_
