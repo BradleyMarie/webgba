99a33aa4858ca4aa5741767d5e08c22088bae5e1
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-03-28 15:28:42 -0700

    Add include guards to generated code

--- tools/bios_data/bios_data_generator.cc
+++ tools/bios_data/bios_data_generator.cc
@@ -15,8 +15,11 @@ std::vector<unsigned char> ReadFile() {
 
 int main(int argc, char* argv[]) {
   std::vector<unsigned char> bytes = ReadFile();
+  std::cout << "#ifndef _TOOLS_BIOS_DATA_DATA_" << std::endl;
+  std::cout << "#define _TOOLS_BIOS_DATA_DATA_" << std::endl;
+
   std::cout << "#include <stdint.h>" << std::endl << std::endl;
-  std::cout << "// generated from " << BIOS_FILE << std::endl;
+
   std::cout << "static const uint32_t bios_size = " << bytes.size() << ";"
             << std::endl;
   std::cout << "static const unsigned char bios_data[" << bytes.size()
@@ -25,5 +28,8 @@ int main(int argc, char* argv[]) {
     std::cout << "  " << (unsigned)byte << "u," << std::endl;
   }
   std::cout << "};" << std::endl;
+
+  std::cout << "#endif  // _TOOLS_BIOS_DATA_DATA_" << std::endl;
+
   return 0;
 }
