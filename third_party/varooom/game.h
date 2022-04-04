8546c96079c5e2d5dde8f1435d3cf599ff3412bc
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-04-04 00:48:45 -0700

    Add homebrew game for testing

---
+++ third_party/varooom/game.h
@@ -0,0 +1,9 @@
+#ifndef _THIRD_PARTY_VAROOOM_GAME_
+#define _THIRD_PARTY_VAROOOM_GAME_
+
+#include <stdint.h>
+
+const unsigned char game_data[7957428];
+const uint32_t game_size;
+
+#endif  // _THIRD_PARTY_VAROOOM_GAME_
