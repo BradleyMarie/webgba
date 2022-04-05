0a84b302297171d79cfda9055fb791a652ef4f77
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-04-04 18:33:32 -0700

    Use GL_NEAREST for filter

--- emulator/ppu/gba/screen.c
+++ emulator/ppu/gba/screen.c
@@ -80,7 +80,7 @@ void GbaPpuScreenReloadContext(GbaPpuScreen* screen) {
 
   glGenTextures(1u, &screen->texture);
   glBindTexture(GL_TEXTURE_2D, screen->texture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
