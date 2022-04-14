9ec826530ff925f8108e05fe2badd59d197f935d
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-04-14 10:51:50 -0700

    Generate game data from binaries directly

--- tools/array_generator/array_generator.cc
+++ tools/array_generator/array_generator.cc
@@ -14,27 +14,22 @@ std::vector<unsigned char> ReadFile(const char* filename) {
 }  // namespace
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cout <<"usage: array_generator <file>" << std::endl;
+  if (argc < 3) {
+    std::cout << "usage: array_generator <file> <array_name>" << std::endl;
     return EXIT_SUCCESS;
   }
 
   std::vector<unsigned char> bytes = ReadFile(argv[1]);
-  std::cout << "#ifndef _TOOLS_ARRAY_GENERATOR_" << std::endl;
-  std::cout << "#define _TOOLS_ARRAY_GENERATOR_" << std::endl << std::endl;
-
   std::cout << "#include <stdint.h>" << std::endl << std::endl;
 
-  std::cout << "static const uint32_t array_size = " << bytes.size() << ";"
+  std::cout << "const uint32_t " << argv[2] << "_size = " << bytes.size() << ";"
             << std::endl;
-  std::cout << "static const unsigned char array_data[" << bytes.size()
+  std::cout << "const unsigned char " << argv[2] << "_data[" << bytes.size()
             << "] = {" << std::endl;
   for (unsigned char byte : bytes) {
     std::cout << "  " << (unsigned)byte << "u," << std::endl;
   }
-  std::cout << "};" << std::endl << std::endl;
-
-  std::cout << "#endif  // _TOOLS_ARRAY_GENERATOR_" << std::endl;
+  std::cout << "};" << std::endl;
 
   return 0;
 }
