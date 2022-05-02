2ea35e4ff3654f09479136324940cf3cb80862e3
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-05-02 02:47:49 -0700

    Switch to scanline rendering instead of dot rendering

--- emulator/ppu/gba/ppu.h
+++ emulator/ppu/gba/ppu.h
@@ -7,6 +7,12 @@
 #include "emulator/memory/memory.h"
 #include "emulator/platform/gba/platform.h"
 
+typedef enum {
+  RENDER_MODE_HARDWARE_ROWS = 0u,
+  RENDER_MODE_SOFTWARE_ROWS = 1u,
+  RENDER_MODE_SOFTWARE_PIXELS = 2u,
+} GbaPpuRenderMode;
+
 typedef struct _GbaPpu GbaPpu;
 
 // General PPU routines
@@ -18,6 +24,9 @@ uint32_t GbaPpuCyclesUntilNextWake(const GbaPpu *ppu);
 
 bool GbaPpuStep(GbaPpu *ppu, uint32_t num_cycles, GLuint fbo,
                 uint8_t scale_factor);
+
+// Rendering mode changes take effect at the beginning of each frame
+void GbaPpuSetRenderMode(GbaPpu *ppu, GbaPpuRenderMode render_mode);
 
 // Context Loss Recovery
 void GbaPpuReloadContext(GbaPpu *ppu);
