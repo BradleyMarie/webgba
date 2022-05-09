4c47c0c796070c68b4139d8b21111854b74d94e7
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-05-09 13:46:48 -0700

    Remove unneeded state from GbaPpuOpenGlBgBitmap

--- emulator/ppu/gba/opengl/bg_bitmap.c
+++ emulator/ppu/gba/opengl/bg_bitmap.c
@@ -2,7 +2,7 @@
 
 #include <string.h>
 
-static void CreateMode34SourceTexture(GLuint* result) {
+static void CreateMode3Texture(GLuint* result) {
   glGenTextures(1u, result);
   glBindTexture(GL_TEXTURE_2D, *result);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -17,20 +17,39 @@ static void CreateMode34SourceTexture(GLuint* result) {
   glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-static void CreateMode5SourceTexture(GLuint* result) {
-  glGenTextures(1u, result);
-  glBindTexture(GL_TEXTURE_2D, *result);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexImage2D(GL_TEXTURE_2D, /*level=*/0, /*internal_format=*/GL_RGBA,
-               /*width=*/GBA_REDUCED_FRAME_WIDTH,
-               /*height=*/GBA_REDUCED_FRAME_HEIGHT,
-               /*border=*/0, /*format=*/GL_RGBA,
-               /*type=*/GL_UNSIGNED_SHORT_5_5_5_1,
-               /*pixels=*/NULL);
-  glBindTexture(GL_TEXTURE_2D, 0);
+static void CreateMode4Textures(GLuint results[2u]) {
+  glGenTextures(2u, results);
+  for (uint8_t i = 0; i < 2u; i++) {
+    glBindTexture(GL_TEXTURE_2D, results[i]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, /*level=*/0, /*internal_format=*/GL_LUMINANCE,
+                 /*width=*/GBA_SCREEN_WIDTH, /*height=*/GBA_SCREEN_HEIGHT,
+                 /*border=*/0, /*format=*/GL_LUMINANCE,
+                 /*type=*/GL_UNSIGNED_BYTE,
+                 /*pixels=*/NULL);
+    glBindTexture(GL_TEXTURE_2D, 0);
+  }
+}
+
+static void CreateMode5Textures(GLuint results[2u]) {
+  glGenTextures(2u, results);
+  for (uint8_t i = 0; i < 2u; i++) {
+    glBindTexture(GL_TEXTURE_2D, results[i]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, /*level=*/0, /*internal_format=*/GL_RGBA,
+                 /*width=*/GBA_REDUCED_FRAME_WIDTH,
+                 /*height=*/GBA_REDUCED_FRAME_HEIGHT,
+                 /*border=*/0, /*format=*/GL_RGBA,
+                 /*type=*/GL_UNSIGNED_SHORT_5_5_5_1,
+                 /*pixels=*/NULL);
+    glBindTexture(GL_TEXTURE_2D, 0);
+  }
 }
 
 static void CreateVertices(GLuint* result) {
@@ -42,77 +61,125 @@ static void CreateVertices(GLuint* result) {
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-static void CreateProgram(GLuint* result) {
-  *result = glCreateProgram();
-
+static GLuint CreateVertexShader() {
   static const char* vertex_shader_source =
       "#version 100\n"
-      "attribute highp vec2 coord;\n"
+      "attribute highp vec2 vertex;\n"
+      "attribute highp vec2 vertextexcoord;\n"
       "varying highp vec2 texcoord;\n"
       "void main() {\n"
-      "  texcoord.x = (coord.x + 1.0) * 0.5;\n"
-      "  texcoord.y = (coord.y - 1.0) * -0.5;\n"
-      "  gl_Position = vec4(coord, 0.0, 1.0);\n"
+      "  texcoord = vertextexcoord;\n"
+      "  gl_Position = vec4(vertex, 0.0, 1.0);\n"
       "}\n";
 
   GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
   glCompileShader(vertex_shader);
 
+  return vertex_shader;
+}
+
+static GLuint CreateMode3FragmentShader() {
   static const char* fragment_shader_source =
       "#version 100\n"
       "uniform lowp sampler2D image;\n"
+      "uniform highp vec2 mosaic;\n"
+      "const highp vec2 screen = vec2(240.0, 160.0);\n"
       "varying highp vec2 texcoord;\n"
       "void main() {\n"
-      "  lowp vec4 color = texture2D(image, texcoord);\n"
+      "  texcoord -= mod(texcoord, mosaic);\n"
+      "  lowp vec4 color = texture2D(image, texcoord / screen);\n"
+      "  color *= step(texcoord.x, screen.x);\n"
+      "  color *= step(texcoord.y, screen.y);\n"
+      "  color *= step(-texcoord.x, 0.0);\n"
+      "  color *= step(-texcoord.y, 0.0);\n"
       "  gl_FragColor = vec4(color.b, color.g, color.r, 0.0);\n"
       "}\n";
 
   GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
   glCompileShader(fragment_shader);
-  glAttachShader(*result, fragment_shader);
-  glAttachShader(*result, vertex_shader);
-  glDeleteShader(fragment_shader);
-  glDeleteShader(vertex_shader);
 
-  glLinkProgram(*result);
+  return fragment_shader;
 }
 
-static void Render(const GbaPpuOpenGlBgBitmap* context,
-                   const GbaPpuMemory* memory, const GbaPpuRegisters* registers,
-                   const GbaPpuInternalRegisters* internal_registers,
-                   const GbaPpuDirtyBits* dirty_bits, GLuint fbo, GLsizei width,
-                   GLsizei height, uint8_t y, GLuint src_texture,
-                   GLsizei bg_width, GLsizei bg_height) {
-  assert(height % GBA_SCREEN_HEIGHT == 0u);
+static GLuint CreateMode4FragmentShader() {
+  static const char* fragment_shader_source =
+      "#version 100\n"
+      "uniform mediump sampler2D image;\n"
+      "uniform highp vec2 mosaic;\n"
+      "uniform lowp sampler2D palette;\n"
+      "const mediump float offset = 1.0 / 512.0;\n"
+      "const highp vec2 screen = vec2(240.0, 160.0);\n"
+      "varying highp vec2 texcoord;\n"
+      "void main() {\n"
+      "  texcoord -= mod(texcoord, mosaic);\n"
+      "  mediump vec4 index = texture2D(image, texcoord / screen);\n"
+      "  lowp vec4 color = texture2D(palette, vec2(index.r + offset, 0.5));\n"
+      "  color *= step(texcoord.x, screen.x);\n"
+      "  color *= step(texcoord.y, screen.y);\n"
+      "  color *= step(-texcoord.x, 0.0);\n"
+      "  color *= step(-texcoord.y, 0.0);\n"
+      "  gl_FragColor = vec4(color.b, color.g, color.r, 0.0);\n"
+      "}\n";
 
-  // TODO: Update vertices
-  GLuint coord_attrib = glGetAttribLocation(context->program, "coord");
-  glBindBuffer(GL_ARRAY_BUFFER, context->vertices);
-  glVertexAttribPointer(coord_attrib, /*size=*/2, /*type=*/GL_FLOAT,
-                        /*normalized=*/false, /*stride=*/0, /*pointer=*/NULL);
-  glEnableVertexAttribArray(coord_attrib);
+  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+  glCompileShader(fragment_shader);
 
-  GLint texture_location = glGetUniformLocation(context->program, "image");
-  glUniform1i(texture_location, 0);
+  return fragment_shader;
+}
 
-  glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_2D, src_texture);
+static GLuint CreateMode5FragmentShader() {
+  static const char* fragment_shader_source =
+      "#version 100\n"
+      "uniform lowp sampler2D image;\n"
+      "uniform highp vec2 mosaic;\n"
+      "const highp vec2 screen = vec2(160.0, 128.0);\n"
+      "varying highp vec2 texcoord;\n"
+      "void main() {\n"
+      "  texcoord -= mod(texcoord, mosaic);\n"
+      "  lowp vec4 color = texture2D(image, texcoord / screen);\n"
+      "  color *= step(texcoord.x, screen.x);\n"
+      "  color *= step(texcoord.y, screen.y);\n"
+      "  color *= step(-texcoord.x, 0.0);\n"
+      "  color *= step(-texcoord.y, 0.0);\n"
+      "  gl_FragColor = vec4(color.b, color.g, color.r, 0.0);\n"
+      "}\n";
 
-  if (dirty_bits->vram.mode_3.overall || dirty_bits->palette.bg_large_palette) {
-    glTexSubImage2D(GL_TEXTURE_2D, /*level=*/0, /*xoffset=*/0, /*yoffset=*/0,
-                    /*width=*/bg_width, /*height=*/bg_height,
-                    /*format=*/GL_RGBA, /*type=*/GL_UNSIGNED_SHORT_5_5_5_1,
-                    /*pixels=*/context->colors);
-  }
+  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+  glCompileShader(fragment_shader);
 
-  GLsizei y_start = height * y / GBA_SCREEN_HEIGHT;
-  GLsizei y_end = height * (y + 1) / GBA_SCREEN_HEIGHT;
+  return fragment_shader;
+}
 
-  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-  glViewport(0, y_start, width, y_end);
-  glDrawArrays(GL_TRIANGLE_FAN, 0, 4u);
+static void CreatePrograms(GLuint* mode3_program, GLuint* mode4_program,
+                           GLuint* mode5_program) {
+  GLuint vertex_shader = CreateVertexShader();
+  GLuint mode3_fragment_shader = CreateMode3FragmentShader();
+  GLuint mode4_fragment_shader = CreateMode4FragmentShader();
+  GLuint mode5_fragment_shader = CreateMode5FragmentShader();
+
+  *mode3_program = glCreateProgram();
+  glAttachShader(mode3_program, vertex_shader);
+  glAttachShader(mode3_program, mode3_fragment_shader);
+  glLinkProgram(mode3_program);
+
+  *mode4_program = glCreateProgram();
+  glAttachShader(mode4_program, vertex_shader);
+  glAttachShader(mode4_program, mode4_fragment_shader);
+  glLinkProgram(mode4_program);
+
+  *mode5_program = glCreateProgram();
+  glAttachShader(mode5_program, vertex_shader);
+  glAttachShader(mode5_program, mode5_fragment_shader);
+  glLinkProgram(mode5_program);
+
+  glDeleteShader(vertex_shader);
+  glDeleteShader(mode3_fragment_shader);
+  glDeleteShader(mode4_fragment_shader);
+  glDeleteShader(mode5_fragment_shader);
 }
 
 void GbaPpuOpenGlBgBitmapMode3(
@@ -121,23 +188,24 @@ void GbaPpuOpenGlBgBitmapMode3(
     const GbaPpuInternalRegisters* internal_registers,
     GbaPpuDirtyBits* dirty_bits, GLuint fbo, GLsizei width, GLsizei height,
     uint8_t y) {
-  if (!dirty_bits->vram.mode_3.overall &&
-      !dirty_bits->palette.bg_large_palette) {
-    return;
-  }
-
-  if (dirty_bits->vram.mode_3.overall || dirty_bits->palette.bg_large_palette) {
+  if (dirty_bits->vram.mode_3.overall) {
     for (uint_fast8_t y = 0; y < GBA_SCREEN_HEIGHT; y++) {
       for (uint_fast8_t x = 0; x < GBA_SCREEN_WIDTH; x++) {
-        context->colors[y * GBA_SCREEN_HEIGHT + x] =
+        context->staging.colors[y * GBA_SCREEN_WIDTH + x] =
             memory->vram.mode_3.bg.pixels[y][x] << 1u;
       }
     }
+
+    glBindTexture(GL_TEXTURE_2D, context->mode3_texture);
+    glTexSubImage2D(GL_TEXTURE_2D, /*level=*/0, /*xoffset=*/0, /*yoffset=*/0,
+                    /*width=*/GBA_SCREEN_WIDTH, /*height=*/GBA_SCREEN_HEIGHT,
+                    /*format=*/GL_RGBA, /*type=*/GL_UNSIGNED_SHORT_5_5_5_1,
+                    /*pixels=*/context->staging.colors);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    dirty_bits->vram.mode_3.overall = false;
   }
 
-  Render(context, memory, registers, internal_registers, dirty_bits, fbo, width,
-         height, y, context->src_texture_mode34, GBA_SCREEN_WIDTH,
-         GBA_SCREEN_HEIGHT);
+  // TODO: Call Render
 }
 
 void GbaPpuOpenGlBgBitmapMode4(
@@ -146,26 +214,27 @@ void GbaPpuOpenGlBgBitmapMode4(
     const GbaPpuInternalRegisters* internal_registers,
     GbaPpuDirtyBits* dirty_bits, GLuint fbo, GLsizei width, GLsizei height,
     uint8_t y) {
-  if (!dirty_bits->vram.mode_3.overall &&
-      !dirty_bits->palette.bg_large_palette) {
-    return;
-  }
-
-  if (dirty_bits->vram.mode_3.overall || dirty_bits->palette.bg_large_palette) {
+  if (dirty_bits->vram.mode_4.pages[registers->dispcnt.page_select]) {
     for (uint_fast8_t y = 0; y < GBA_SCREEN_HEIGHT; y++) {
       for (uint_fast8_t x = 0; x < GBA_SCREEN_WIDTH; x++) {
-        uint8_t color_index =
+        context->staging.indices[y * GBA_SCREEN_WIDTH + x] =
             memory->vram.mode_4.bg.pages[registers->dispcnt.page_select]
                 .pixels[y][x];
-        context->colors[y * GBA_SCREEN_HEIGHT + x] =
-            memory->palette.bg.large_palette[color_index] << 1u;
       }
     }
+
+    glBindTexture(GL_TEXTURE_2D,
+                  context->mode4_textures[registers->dispcnt.page_select]);
+    glTexSubImage2D(GL_TEXTURE_2D, /*level=*/0, /*xoffset=*/0, /*yoffset=*/0,
+                    /*width=*/GBA_SCREEN_WIDTH, /*height=*/GBA_SCREEN_HEIGHT,
+                    /*format=*/GL_LUMINANCE, /*type=*/GL_UNSIGNED_BYTE,
+                    /*pixels=*/context->staging.indices);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    dirty_bits->vram.mode_4.pages[registers->dispcnt.page_select] = false;
   }
 
-  Render(context, memory, registers, internal_registers, dirty_bits, fbo, width,
-         height, y, context->src_texture_mode34, GBA_SCREEN_WIDTH,
-         GBA_SCREEN_HEIGHT);
+  // TODO: Call Render
 }
 
 void GbaPpuOpenGlBgBitmapMode5(
@@ -174,35 +243,45 @@ void GbaPpuOpenGlBgBitmapMode5(
     const GbaPpuInternalRegisters* internal_registers,
     GbaPpuDirtyBits* dirty_bits, GLuint fbo, GLsizei width, GLsizei height,
     uint8_t y) {
-  if (!dirty_bits->vram.mode_3.overall &&
-      !dirty_bits->palette.bg_large_palette) {
-    return;
-  }
-
-  if (dirty_bits->vram.mode_3.overall || dirty_bits->palette.bg_large_palette) {
+  if (dirty_bits->vram.mode_5.pages[registers->dispcnt.page_select]) {
     for (uint_fast8_t y = 0; y < GBA_REDUCED_FRAME_HEIGHT; y++) {
       for (uint_fast8_t x = 0; x < GBA_REDUCED_FRAME_WIDTH; x++) {
-        context->colors[y * GBA_REDUCED_FRAME_HEIGHT + x] =
-            memory->vram.mode_3.bg.pixels[y][x] << 1u;
+        context->staging.colors[y * GBA_REDUCED_FRAME_WIDTH + x] =
+            memory->vram.mode_5.bg.pages[registers->dispcnt.page_select]
+                .pixels[y][x];
       }
     }
+
+    glBindTexture(GL_TEXTURE_2D,
+                  context->mode5_textures[registers->dispcnt.page_select]);
+    glTexSubImage2D(GL_TEXTURE_2D, /*level=*/0, /*xoffset=*/0, /*yoffset=*/0,
+                    /*width=*/GBA_REDUCED_FRAME_WIDTH,
+                    /*height=*/GBA_REDUCED_FRAME_HEIGHT,
+                    /*format=*/GL_RGBA, /*type=*/GL_UNSIGNED_SHORT_5_5_5_1,
+                    /*pixels=*/context->staging.colors);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    dirty_bits->vram.mode_5.pages[registers->dispcnt.page_select] = false;
   }
 
-  Render(context, memory, registers, internal_registers, dirty_bits, fbo, width,
-         height, y, context->src_texture_mode5, GBA_REDUCED_FRAME_WIDTH,
-         GBA_REDUCED_FRAME_HEIGHT);
+  // TODO: Call Render
 }
 
 void GbaPpuOpenGlBgBitmapReloadContext(GbaPpuOpenGlBgBitmap* context) {
-  CreateMode34SourceTexture(&context->src_texture_mode34);
-  CreateMode5SourceTexture(&context->src_texture_mode5);
-  CreateProgram(&context->program);
   CreateVertices(&context->vertices);
+  CreateMode3Texture(&context->mode3_texture);
+  CreateMode4Texture(context->mode4_textures);
+  CreateMode5Texture(context->mode5_textures);
+  CreatePrograms(&context->mode3_program, &context->mode4_program,
+                 &context->mode5_program);
 }
 
 void GbaPpuOpenGlBgBitmapDestroy(GbaPpuOpenGlBgBitmap* context) {
-  glDeleteTextures(1u, &context->src_texture_mode34);
-  glDeleteTextures(1u, &context->src_texture_mode5);
-  glDeleteProgram(context->program);
   glDeleteBuffers(1u, &context->vertices);
+  glDeleteTextures(1u, &context->mode3_texture);
+  glDeleteTextures(2u, context->mode4_textures);
+  glDeleteTextures(2u, context->mode5_textures);
+  glDeleteProgram(context->mode3_program);
+  glDeleteProgram(context->mode4_program);
+  glDeleteProgram(context->mode5_program);
 }
