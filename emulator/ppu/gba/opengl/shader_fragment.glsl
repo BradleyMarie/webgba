eb64dbe113d558f637af8930131357eb503bd318
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-05-23 23:57:05 -0700

    Inline scrolling bg function

--- emulator/ppu/gba/opengl/shader_fragment.glsl
+++ emulator/ppu/gba/opengl/shader_fragment.glsl
@@ -424,20 +424,22 @@ WindowContents CheckWindow(bool on_object) {
   return result;
 }
 
-// Scrolling Tile Map
-struct ScrollingTilemapEntry {
-  highp float tile_block_position;
-  lowp float palette;
-  lowp vec2 flip;
-};
-
-ScrollingTilemapEntry GetScrollingTileMapEntry(highp float tilemap_base,
-                                               highp vec2 tilemap_size_pixels,
-                                               highp vec2 tilemap_pixel) {
+// Scrolling Backgrounds
+lowp vec4 ScrollingBackgroundImpl(highp float tilemap_base,
+                                  highp vec2 tilemap_size_pixels,
+                                  highp vec2 tilemap_pixel, highp vec2 mosaic,
+                                  highp float tile_base, bool large_palette) {
   const highp float number_of_tilemap_blocks = 32.0;
-  const highp float tile_size_pixels = 8.0;
+  const highp float tile_size = 8.0;
   const highp float tilemap_block_size_pixels =
-      number_of_tilemap_blocks * tile_size_pixels;
+      number_of_tilemap_blocks * tile_size;
+
+  mediump float num_tiles = large_palette ? 1024.0 : 2048.0;
+  mediump float tile_block_divisor = large_palette ? 1.0 : 2.0;
+
+  tilemap_pixel = mod(tilemap_pixel, tilemap_size_pixels);
+  tilemap_pixel -= mod(tilemap_pixel, mosaic);
+  tilemap_pixel += vec2(0.5, 0.5);
 
   highp vec2 tilemap_block_index_2d =
       floor(tilemap_pixel / tilemap_block_size_pixels);
@@ -450,49 +452,27 @@ ScrollingTilemapEntry GetScrollingTileMapEntry(highp float tilemap_base,
   highp vec2 tilemap_block_pixel =
       mod(tilemap_pixel, tilemap_block_size_pixels) / tilemap_block_size_pixels;
 
-  highp vec2 lookup =
+  highp vec2 tile =
       vec2(tilemap_block_pixel.x,
            tilemap_base + (tilemap_block_pixel.y + tilemap_block_index_1d) /
                               number_of_tilemap_blocks);
 
-  highp vec4 indices = texture2D(bg_scrolling_tilemap_indices, lookup);
-  lowp vec4 params = texture2D(bg_scrolling_tilemap_params, lookup);
+  highp vec4 indices = texture2D(bg_scrolling_tilemap_indices, tile);
+  lowp vec4 params = texture2D(bg_scrolling_tilemap_params, tile);
 
-  ScrollingTilemapEntry result;
-  result.palette = (params.z * 31.0) / 32.0;
-  result.flip = params.xy;
-  result.tile_block_position =
-    (indices.a * 255.0 * 256.0 + indices.r * 255.0) / 1024.0;
+  mediump float tile_block_position =
+      (indices.a * 255.0 * 256.0 + indices.r * 255.0) / 1024.0;
 
-  return result;
-}
-
-lowp vec4 ScrollingBackgroundImpl(highp float tilemap_base,
-                                  highp vec2 tilemap_size_pixels,
-                                  highp vec2 tilemap_pixel, highp vec2 mosaic,
-                                  highp float tile_base, bool large_palette) {
-  mediump float num_tiles = large_palette ? 1024.0 : 2048.0;
-  mediump float tile_block_divisor = large_palette ? 1.0 : 2.0;
-
-  highp vec2 wrapped_tilemap_pixel = mod(tilemap_pixel, tilemap_size_pixels);
-  highp vec2 lookup = wrapped_tilemap_pixel -
-                      mod(wrapped_tilemap_pixel, mosaic) + vec2(0.5, 0.5);
-
-  ScrollingTilemapEntry entry =
-      GetScrollingTileMapEntry(tilemap_base, tilemap_size_pixels, lookup);
-
-  const highp float tile_size = 8.0;
-  highp vec2 tile_pixel = mod(lookup, tile_size) / tile_size;
-
-  tile_pixel = abs(entry.flip - tile_pixel);
+  highp vec2 tile_pixel = mod(tilemap_pixel, tile_size) / tile_size;
+  tile_pixel = abs(params.xy - tile_pixel);
 
   lowp vec4 color_indices = texture2D(
-      bg_tiles, vec2(tile_pixel.x,
-                  tile_base + entry.tile_block_position / tile_block_divisor +
-                      tile_pixel.y / num_tiles));
+      bg_tiles,
+      vec2(tile_pixel.x, tile_base + tile_block_position / tile_block_divisor +
+                             tile_pixel.y / num_tiles));
   lowp float color_index = large_palette ? color_indices.r : color_indices.a;
 
-  lowp float palette = large_palette ? 0.0 : entry.palette;
+  lowp float palette = large_palette ? 0.0 : (params.z * 31.0) / 32.0;
   lowp float palette_offset = (color_index * 255.0 + 0.5) / 256.0;
   lowp vec4 color = texture2D(bg_palette, vec2(palette + palette_offset, 0.5));
   return vec4(color.rgb, sign(color_index));
@@ -522,16 +502,19 @@ lowp vec4 ScrollingBackground3() {
                                  bg3_tile_base, bg3_large_palette);
 }
 
+// Affine Backgrounds
 lowp vec4 AffineBackgroundImpl(highp float tilemap_base,
                                highp vec2 tilemap_size_pixels,
                                highp vec2 tilemap_pixel, highp vec2 mosaic,
                                highp float tile_base, bool wraparound) {
+  const highp float num_tiles = 1024.0;
+  const highp float tile_size = 8.0;
+
   highp vec2 wrapped_tilemap_pixel =
       wraparound ? mod(tilemap_pixel, tilemap_size_pixels) : tilemap_pixel;
   highp vec2 lookup_pixel =
       wrapped_tilemap_pixel - mod(wrapped_tilemap_pixel, mosaic);
 
-  const highp float tile_size = 8.0;
   highp vec2 lookup_tile = floor(lookup_pixel / tile_size);
   highp vec2 tilemap_size_tiles = tilemap_size_pixels / tile_size;
 
@@ -545,7 +528,6 @@ lowp vec4 AffineBackgroundImpl(highp float tilemap_base,
 
   highp vec2 tile_pixel = mod(lookup_pixel, tile_size) / tile_size;
 
-  const highp float num_tiles = 1024.0;
   lowp vec4 color_indices = texture2D(
       bg_tiles,
       vec2(tile_pixel.x, tile_base + index / 4.0 + tile_pixel.y / num_tiles));
