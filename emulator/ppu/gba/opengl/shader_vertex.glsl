67944adb870f60833179bb9b49a45245d47f8e42
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-05-26 20:12:01 -0700

    Fix incorrect scrolling background values

--- emulator/ppu/gba/opengl/shader_vertex.glsl
+++ emulator/ppu/gba/opengl/shader_vertex.glsl
@@ -21,8 +21,8 @@ varying highp vec2 screencoord;
 void main() {
   bg0_scrolling_screencoord = bg0_scrolling;
   bg1_scrolling_screencoord = bg1_scrolling;
-  bg2_scrolling_screencoord = bg1_scrolling;
-  bg3_scrolling_screencoord = bg2_scrolling;
+  bg2_scrolling_screencoord = bg2_scrolling;
+  bg3_scrolling_screencoord = bg3_scrolling;
   bg2_affine_screencoord = bg2_affine;
   bg3_affine_screencoord = bg3_affine;
   screencoord.x = (vertex.x + 1.0) * 120.0;
